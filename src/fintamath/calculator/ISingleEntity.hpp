99e70730f04c194d45cc065b5ed8aaf286f1dc1a
fintarin <valukevichv@gmail.com>
2021-10-22 19:23:37 +0300

    Add constructors to ISingleEntity

--- src/fintamath/calculator/ISingleEntity.hpp
+++ src/fintamath/calculator/ISingleEntity.hpp
@@ -4,9 +4,20 @@
 #include <string>
 
 class ISingleEntity {
+protected:
+  ISingleEntity() = default;
+  ISingleEntity(ISingleEntity &&rhs) = default;
+  ISingleEntity(const ISingleEntity &rhs) = default;
+  ISingleEntity &operator=(ISingleEntity &&rhs) = default;
+  ISingleEntity &operator=(const ISingleEntity &rhs) = default;
+
 public:
+  virtual ~ISingleEntity() = 0;
+
   virtual std::string getTypeName() const = 0;
   virtual std::string toString() const = 0;
 };
+
+inline ISingleEntity::~ISingleEntity() = default;
 
 #endif // ISINGLEENTITY_HPP
