1ec147c060ca7893b6dc9a61900d3bdd3296c7a3
fintarin <valukevichv@gmail.com>
2021-10-23 18:17:52 +0300

    Implement tests to cover all math functions

--- src/tests/src/CalculatorTests.cpp
+++ src/tests/src/CalculatorTests.cpp
@@ -20,8 +20,15 @@ TEST(CalculatorTests, calculationNegativeTests) {
   for (string input, output; getline(testsIn, input);) {
     cout << input << '\n' << output << "\n\n";
     EXPECT_ANY_THROW(calc.calculate(input));
-    getline(testsIn, input);
   }
+}
+
+TEST(CalculatorTests, getETest) {
+  const int precision = 100;
+  Calculator calc;
+  calc.setPrecision(100);
+  EXPECT_EQ(calc.calculate("e"),
+            "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274");
 }
 
 TEST(CalculatorTests, getSetPrecisionTest) {
@@ -29,4 +36,6 @@ TEST(CalculatorTests, getSetPrecisionTest) {
   Calculator calc;
   calc.setPrecision(100);
   EXPECT_EQ(calc.getPrecision(), precision);
+  calc.setPrecision(-1);
+  EXPECT_EQ(calc.getPrecision(), 1);
 }
