184912773447bd70d7e75d62e5d196e16909c7b6
fintarin <valukevichv@gmail.com>
2022-01-29 16:30:44 +0300

    Add operator() to relation

--- src/math_objects/src/fintamath/math_objects/relations/Relation.hpp
+++ src/math_objects/src/fintamath/math_objects/relations/Relation.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "fintamath/math_objects/MathObject.hpp"
+#include "fintamath/math_objects/nodes/collections/Set.hpp"
 
 namespace fintamath {
   class Relation : public MathObject {
   public:
+    virtual Set operator()(const Set &set, int64_t precision = 0) const = 0;
     ~Relation() override = default;
   };
 
