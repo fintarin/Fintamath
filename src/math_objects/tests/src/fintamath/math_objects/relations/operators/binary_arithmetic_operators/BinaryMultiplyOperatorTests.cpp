79bbdb15d8dbc9254969694540ca715d99d2128d
fintarin <valukevichv@gmail.com>
2022-02-13 01:44:49 +0300

    Implement BinaryMultiplyOperator

---
+++ src/math_objects/tests/src/fintamath/math_objects/relations/operators/binary_arithmetic_operators/BinaryMultiplyOperatorTests.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/binary_arithmetic_operators/BinaryMultiplyOperator.hpp"
+
+TEST(BinaryMultiplyOperatorTests, callOperatorTest) {
+}
+
+TEST(BinaryMultiplyOperatorTests, intIntCallOperatorTest) {
+}
+
+TEST(BinaryMultiplyOperatorTests, ratRatCallOperatorTest) {
+}
+
+TEST(BinaryMultiplyOperatorTests, intRatCallOperatorTest) {
+}
+
+TEST(BinaryMultiplyOperatorTests, ratIntCallOperatorTest) {
+}
+
+TEST(BinaryMultiplyOperatorTests, toStringTest) {
+}
