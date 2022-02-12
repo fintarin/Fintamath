8efacb23daef4b57f73085d74ed626c7d614c6f5
fintarin <valukevichv@gmail.com>
2022-02-13 01:18:47 +0300

    Implement RelationParser

---
+++ src/math_objects/src/fintamath/math_objects/relations/RelationParser.cpp
@@ -0,0 +1,11 @@
+#include "fintamath/math_objects/relations/RelationParser.hpp"
+
+#include <stdexcept>
+
+#include "fintamath/math_objects/nodes/terms/TermParser.hpp"
+
+using namespace fintamath;
+
+RelationPtr RelationParser::parse(const std::string_view & /*str*/) {
+  return nullptr;
+}
