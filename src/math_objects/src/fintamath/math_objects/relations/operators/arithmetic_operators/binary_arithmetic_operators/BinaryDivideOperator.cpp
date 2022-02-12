bf8559c47553d6fcf3278b82c1aa5b13fda473bf
fintarin <valukevichv@gmail.com>
2022-02-13 01:45:58 +0300

    Implement BinaryDivideOperator

---
+++ src/math_objects/src/fintamath/math_objects/relations/operators/arithmetic_operators/binary_arithmetic_operators/BinaryDivideOperator.cpp
@@ -0,0 +1,42 @@
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/binary_arithmetic_operators/BinaryDivideOperator.hpp"
+
+#include <stdexcept>
+
+#include "fintamath/math_objects/relations/RelationInjector.hpp"
+
+using namespace fintamath;
+
+template <typename T1, typename T2> static NodePtr divide(const T1 &lhs, const T2 &rhs);
+
+Set BinaryDivideOperator::operator()(const Set &set, int64_t /*precision*/) const {
+  INJECT_BINARY_RELATION(Integer, Integer, set)
+  INJECT_BINARY_RELATION(Rational, Rational, set)
+  INJECT_BINARY_RELATION(Integer, Rational, set)
+  INJECT_BINARY_RELATION(Rational, Integer, set)
+  throw std::invalid_argument("");
+}
+
+NodePtr BinaryDivideOperator::operator()(const Integer &lhs, const Integer &rhs) const {
+  return divide(lhs, rhs);
+}
+
+NodePtr BinaryDivideOperator::operator()(const Rational &lhs, const Rational &rhs) const {
+  return divide(lhs, rhs);
+}
+
+NodePtr BinaryDivideOperator::operator()(const Integer &lhs, const Rational &rhs) const {
+  return divide(Rational(lhs), rhs);
+}
+
+NodePtr BinaryDivideOperator::operator()(const Rational &lhs, const Integer &rhs) const {
+  return divide(lhs, Rational(rhs));
+}
+
+std::string BinaryDivideOperator::toString() const {
+  return "/";
+}
+
+template <typename T1, typename T2> static NodePtr divide(const T1 &lhs, const T2 &rhs) {
+  auto res = lhs / rhs;
+  return std::make_shared<decltype(res)>(res);
+}
