c992ee20c9e3ebcdbed7b8aeebc4faef260411e1
fintarin <valukevichv@gmail.com>
2022-02-13 01:56:37 +0300

    Implement UnaryMinusOperator

---
+++ src/math_objects/tests/src/fintamath/math_objects/relations/operators/unary_arithmetic_operators/UnaryMinusOperatorTests.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/unary_arithmetic_operators/UnaryMinusOperator.hpp"
+
+TEST(UnaryMinusOperatorTests, callOperatorTest) {
+}
+
+TEST(UnaryMinusOperatorTests, intCallOperatorTest) {
+}
+
+TEST(UnaryMinusOperatorTests, ratCallOperatorTest) {
+}
+
+TEST(UnaryMinusOperatorTests, toStringTest) {
+}
