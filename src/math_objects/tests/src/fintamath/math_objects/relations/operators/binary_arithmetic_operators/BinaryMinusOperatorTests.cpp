93cf9e304470a88de861ae19e75ac316e969012d
fintarin <valukevichv@gmail.com>
2022-02-13 01:43:36 +0300

    Implement BinaryMinusOperator

---
+++ src/math_objects/tests/src/fintamath/math_objects/relations/operators/binary_arithmetic_operators/BinaryMinusOperatorTests.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/binary_arithmetic_operators/BinaryMinusOperator.hpp"
+
+TEST(BinaryMinusOperatorTests, callOperatorTest) {
+}
+
+TEST(BinaryMinusOperatorTests, intIntCallOperatorTest) {
+}
+
+TEST(BinaryMinusOperatorTests, ratRatCallOperatorTest) {
+}
+
+TEST(BinaryMinusOperatorTests, intRatCallOperatorTest) {
+}
+
+TEST(BinaryMinusOperatorTests, ratIntCallOperatorTest) {
+}
+
+TEST(BinaryMinusOperatorTests, toStringTest) {
+}
