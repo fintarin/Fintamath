60ed9f9c9265e610945790fd31e41ff5d0fe652c
fintarin <valukevichv@gmail.com>
2022-02-13 01:56:04 +0300

    Implement UnaryPlusOperator

---
+++ src/math_objects/src/fintamath/math_objects/relations/operators/arithmetic_operators/unary_arithmetic_operators/UnaryPlusOperator.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "fintamath/math_objects/nodes/terms/numbers/Rational.hpp"
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/ArithmeticOperator.hpp"
+
+namespace fintamath {
+  class UnaryPlusOperator : public ArithmeticOperator {
+  public:
+    Set operator()(const Set &set, int64_t precision = 0) const override;
+
+    NodePtr operator()(const Integer &rhs) const;
+    NodePtr operator()(const Rational &rhs) const;
+
+    std::string toString() const override;
+  };
+}
---
+++ src/math_objects/tests/src/fintamath/math_objects/relations/operators/unary_arithmetic_operators/UnaryPlusOperatorTests.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/unary_arithmetic_operators/UnaryPlusOperator.hpp"
+
+TEST(UnaryPlusOperatorTests, callOperatorTest) {
+}
+
+TEST(UnaryPlusOperatorTests, intCallOperatorTest) {
+}
+
+TEST(UnaryPlusOperatorTests, ratCallOperatorTest) {
+}
+
+TEST(UnaryPlusOperatorTests, toStringTest) {
+}
