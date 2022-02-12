27b8ecd501e0bccde8a48a436b8bdacf515bac98
fintarin <valukevichv@gmail.com>
2022-02-13 01:44:49 +0300

    Implement BinaryMultiplyOperator

---
+++ src/math_objects/src/fintamath/math_objects/relations/operators/arithmetic_operators/binary_arithmetic_operators/BinaryMultiplyOperator.cpp
@@ -0,0 +1,42 @@
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/binary_arithmetic_operators/BinaryMultiplyOperator.hpp"
+
+#include <stdexcept>
+
+#include "fintamath/math_objects/relations/RelationUtility.hpp"
+
+using namespace fintamath;
+
+template <typename T1, typename T2> static NodePtr multiply(const T1 &lhs, const T2 &rhs);
+
+Set BinaryMultiplyOperator::operator()(const Set &set, int64_t /*precision*/) const {
+  INJECT_BINARY_RELATION(Integer, Integer, set)
+  INJECT_BINARY_RELATION(Rational, Rational, set)
+  INJECT_BINARY_RELATION(Integer, Rational, set)
+  INJECT_BINARY_RELATION(Rational, Integer, set)
+  throw std::invalid_argument("");
+}
+
+NodePtr BinaryMultiplyOperator::operator()(const Integer &lhs, const Integer &rhs) const {
+  return multiply(lhs, rhs);
+}
+
+NodePtr BinaryMultiplyOperator::operator()(const Rational &lhs, const Rational &rhs) const {
+  return multiply(lhs, rhs);
+}
+
+NodePtr BinaryMultiplyOperator::operator()(const Integer &lhs, const Rational &rhs) const {
+  return multiply(Rational(lhs), rhs);
+}
+
+NodePtr BinaryMultiplyOperator::operator()(const Rational &lhs, const Integer &rhs) const {
+  return multiply(lhs, Rational(rhs));
+}
+
+std::string BinaryMultiplyOperator::toString() const {
+  return "*";
+}
+
+template <typename T1, typename T2> static NodePtr multiply(const T1 &lhs, const T2 &rhs) {
+  auto res = lhs * rhs;
+  return std::make_shared<decltype(res)>(res);
+}
