2824c01c812b6b52eccf507fe3baa2ae066c1203
fintarin <valukevichv@gmail.com>
2022-02-13 01:39:44 +0300

    Add arithmetic operator interfaces

---
+++ src/math_objects/src/fintamath/math_objects/relations/operators/arithmetic_operators/ArithmeticOperator.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "fintamath/math_objects/relations/operators/Operator.hpp"
+
+namespace fintamath {
+  class ArithmeticOperator : public Operator {
+  public:
+    ~ArithmeticOperator() override = default;
+  };
+}
