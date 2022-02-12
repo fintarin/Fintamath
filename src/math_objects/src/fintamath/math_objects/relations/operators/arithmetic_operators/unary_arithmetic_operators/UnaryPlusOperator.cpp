dd79cda194061b7ff2f6caff7dc33f7574b5b68b
fintarin <valukevichv@gmail.com>
2022-02-13 01:56:04 +0300

    Implement UnaryPlusOperator

---
+++ src/math_objects/src/fintamath/math_objects/relations/operators/arithmetic_operators/unary_arithmetic_operators/UnaryPlusOperator.cpp
@@ -0,0 +1,31 @@
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/unary_arithmetic_operators/UnaryPlusOperator.hpp"
+
+#include <stdexcept>
+
+#include "fintamath/math_objects/relations/RelationUtility.hpp"
+
+using namespace fintamath;
+
+template <typename T> static NodePtr convert(const T &rhs);
+
+Set UnaryPlusOperator::operator()(const Set &set, int64_t /*precision*/) const {
+  INJECT_UNARY_RELATION(Integer, set)
+  INJECT_UNARY_RELATION(Rational, set)
+  throw std::invalid_argument("");
+}
+
+NodePtr UnaryPlusOperator::operator()(const Integer &obj) const {
+  return std::make_shared<Integer>(obj);
+}
+
+NodePtr UnaryPlusOperator::operator()(const Rational &obj) const {
+  return std::make_shared<Rational>(obj);
+}
+
+std::string UnaryPlusOperator::toString() const {
+  return "+";
+}
+
+template <typename T> static NodePtr convert(const T &rhs) {
+  return std::make_shared<T>(+rhs);
+}
