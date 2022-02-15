d69429ad6a6c0055533281ddc7c637d3f8b7a7ec
fintarin <valukevichv@gmail.com>
2022-02-15 16:22:36 +0300

    Implement parser tests

--- src/math_objects/tests/src/fintamath/math_objects/nodes/NodeParserTests.cpp
+++ src/math_objects/tests/src/fintamath/math_objects/nodes/NodeParserTests.cpp
@@ -2,5 +2,9 @@
 
 #include "fintamath/math_objects/nodes/NodeParser.hpp"
 
+using namespace fintamath;
+
 TEST(NodeParserTests, parseTest) {
+  EXPECT_EQ(NodeParser::parse("1")->toString(), "1");
+  EXPECT_EQ(NodeParser::parse("+"), nullptr);
 }
--- src/math_objects/tests/src/fintamath/math_objects/nodes/terms/numbers/NumberParserTests.cpp
+++ src/math_objects/tests/src/fintamath/math_objects/nodes/terms/numbers/NumberParserTests.cpp
@@ -2,5 +2,10 @@
 
 #include "fintamath/math_objects/nodes/terms/numbers/NumberParser.hpp"
 
+using namespace fintamath;
+
 TEST(NumberParserTests, parseTest) {
+  EXPECT_EQ(NumberParser::parse("1")->toString(), "1");
+  EXPECT_EQ(NumberParser::parse("2.2")->toString(), "11/5");
+  EXPECT_EQ(NumberParser::parse("2.2.2"), nullptr);
 }
