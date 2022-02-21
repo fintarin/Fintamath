f362141557f93229ed1c751fcedd322d0b77558f
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Implement unary arithmetic operator tests

--- src/math_objects/tests/src/fintamath/math_objects/relations/operators/unary_arithmetic_operators/UnaryMinusOperatorTests.cpp
+++ src/math_objects/tests/src/fintamath/math_objects/relations/operators/unary_arithmetic_operators/UnaryMinusOperatorTests.cpp
@@ -2,14 +2,26 @@
 
 #include "fintamath/math_objects/relations/operators/arithmetic_operators/unary_arithmetic_operators/UnaryMinusOperator.hpp"
 
+using namespace fintamath;
+
+const UnaryMinusOperator oper;
+
 TEST(UnaryMinusOperatorTests, callOperatorTest) {
+  EXPECT_EQ(*oper({std::make_shared<Integer>(20)}).at(0), Integer(-20));
+  EXPECT_EQ(*oper({std::make_shared<Rational>(2, 3)}).at(0), Rational(-2, 3));
+
+  auto s = std::make_shared<Set>();
+  EXPECT_THROW(oper({s}), std::invalid_argument);
 }
 
 TEST(UnaryMinusOperatorTests, intCallOperatorTest) {
+  EXPECT_EQ(*oper(Integer(4574234)), Integer(-4574234));
 }
 
 TEST(UnaryMinusOperatorTests, ratCallOperatorTest) {
+  EXPECT_EQ(*oper(Rational(4574234, 48483532)), Rational(-4574234, 48483532));
 }
 
 TEST(UnaryMinusOperatorTests, toStringTest) {
+  EXPECT_EQ(oper.toString(), "-");
 }
