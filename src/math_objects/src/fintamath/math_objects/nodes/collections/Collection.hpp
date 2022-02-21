96f5e3c2f65f6d79a4c91ed5f93bec3beb2b9c79
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Add collection

---
+++ src/math_objects/src/fintamath/math_objects/nodes/collections/Collection.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "fintamath/math_objects/nodes/Node.hpp"
+
+namespace fintamath {
+  class Collection : public Node {
+  public:
+    ~Collection() override = default;
+  };
+}
