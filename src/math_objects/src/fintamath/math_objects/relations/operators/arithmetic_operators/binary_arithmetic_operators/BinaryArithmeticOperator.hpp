580e616af59fc5f6ecc3e044310bd9e2b74c2f51
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Add arithmetic operator interfaces

---
+++ src/math_objects/src/fintamath/math_objects/relations/operators/arithmetic_operators/binary_arithmetic_operators/BinaryArithmeticOperator.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/ArithmeticOperator.hpp"
+
+namespace fintamath {
+  class BinaryArithmeticOperator : public ArithmeticOperator {
+  public:
+    ~BinaryArithmeticOperator() override = default;
+  };
+}
