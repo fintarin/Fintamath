d718bef816a7af72168716c356f73917780db90b
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Implement NodeParser

---
+++ src/math_objects/src/fintamath/math_objects/nodes/NodeParser.cpp
@@ -0,0 +1,19 @@
+#include "fintamath/math_objects/nodes/NodeParser.hpp"
+
+#include <stdexcept>
+
+#include "fintamath/math_objects/nodes/collections/CollectionParser.hpp"
+#include "fintamath/math_objects/nodes/expressions/ExpressionParser.hpp"
+#include "fintamath/math_objects/nodes/terms/TermParser.hpp"
+
+using namespace fintamath;
+
+NodePtr NodeParser::parse(const std::string_view &str) {
+  if (auto res = TermParser::parse(str); res) {
+    return res;
+  }
+  if (auto res = CollectionParser::parse(str); res) {
+    return res;
+  }
+  return ExpressionParser::parse(str);
+}
