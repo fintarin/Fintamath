c76efabfb24be17c25f7c9536d5b13c0019cebab
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Implement BinaryPlusOperator

---
+++ src/math_objects/tests/src/fintamath/math_objects/relations/operators/binary_arithmetic_operators/BinaryPlusOperatorTests.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/binary_arithmetic_operators/BinaryPlusOperator.hpp"
+
+TEST(BinaryPlusOperatorTests, callOperatorTest) {
+}
+
+TEST(BinaryPlusOperatorTests, intIntCallOperatorTest) {
+}
+
+TEST(BinaryPlusOperatorTests, ratRatCallOperatorTest) {
+}
+
+TEST(BinaryPlusOperatorTests, intRatCallOperatorTest) {
+}
+
+TEST(BinaryPlusOperatorTests, ratIntCallOperatorTest) {
+}
+
+TEST(BinaryPlusOperatorTests, toStringTest) {
+}
