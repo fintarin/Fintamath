08421abbd62fb62e772d911dfc19de74ed51cfce
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Implement NumberParser

---
+++ src/math_objects/src/fintamath/math_objects/nodes/terms/numbers/NumberParser.cpp
@@ -0,0 +1,21 @@
+#include "fintamath/math_objects/nodes/terms/numbers/NumberParser.hpp"
+
+#include <stdexcept>
+
+#include "fintamath/math_objects/nodes/terms/numbers/Rational.hpp"
+
+using namespace fintamath;
+
+NodePtr NumberParser::parse(const std::string_view &str) {
+  try {
+    return std::make_shared<Integer>(str);
+  } catch (const std::invalid_argument &) {
+    // do nothing
+  }
+  try {
+    return std::make_shared<Rational>(str);
+  } catch (const std::invalid_argument &) {
+    // do nothing
+  }
+  return nullptr;
+}
---
+++ src/math_objects/tests/src/fintamath/math_objects/nodes/terms/numbers/NumberParserTests.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/math_objects/nodes/terms/numbers/NumberParser.hpp"
+
+TEST(NumberParserTests, parseTest) {
+}
