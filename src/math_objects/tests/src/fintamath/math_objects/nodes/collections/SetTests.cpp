e8a73ff5e08b69b0227e924ca832366a35577c8e
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Add dummy Set

---
+++ src/math_objects/tests/src/fintamath/math_objects/nodes/collections/SetTests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/math_objects/nodes/collections/Set.hpp"
+
+TEST(SetTests, stringConstructorTest) {
+}
+
+TEST(SetTests, initializerListConstructorTest) {
+}
+
+TEST(SetTests, toStringTest) {
+}
