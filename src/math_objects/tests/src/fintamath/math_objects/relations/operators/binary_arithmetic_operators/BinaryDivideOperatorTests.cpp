d95171ab3d1d346284096783aa25300a5841b858
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Implement binary arithmetic operator tests

--- src/math_objects/tests/src/fintamath/math_objects/relations/operators/binary_arithmetic_operators/BinaryDivideOperatorTests.cpp
+++ src/math_objects/tests/src/fintamath/math_objects/relations/operators/binary_arithmetic_operators/BinaryDivideOperatorTests.cpp
@@ -2,20 +2,37 @@
 
 #include "fintamath/math_objects/relations/operators/arithmetic_operators/binary_arithmetic_operators/BinaryDivideOperator.hpp"
 
+using namespace fintamath;
+
+const BinaryDivideOperator oper;
+
 TEST(BinaryDivideOperatorTests, callOperatorTest) {
+  EXPECT_EQ(*oper({std::make_shared<Integer>(20), std::make_shared<Integer>(10)}).at(0), Integer(2));
+  EXPECT_EQ(*oper({std::make_shared<Rational>(2, 3), std::make_shared<Rational>(3, 4)}).at(0), Rational(8, 9));
+  EXPECT_EQ(*oper({std::make_shared<Integer>(20), std::make_shared<Rational>(3, 4)}).at(0), Rational(80, 3));
+  EXPECT_EQ(*oper({std::make_shared<Rational>(2, 3), std::make_shared<Integer>(10)}).at(0), Rational(1, 15));
+
+  auto s = std::make_shared<Set>();
+  EXPECT_THROW(oper({s, s}), std::invalid_argument);
 }
 
 TEST(BinaryDivideOperatorTests, intIntCallOperatorTest) {
+  EXPECT_EQ(*oper(Integer("437826432987462398746289734"), Integer("923749326428379569328756")), Integer(473));
 }
 
 TEST(BinaryDivideOperatorTests, ratRatCallOperatorTest) {
+  EXPECT_EQ(*oper(Rational(9825742, 532437), Rational(3445466, 100000000)), Rational(491287100000000, 917246790321));
+  EXPECT_EQ(*oper(Rational(3, 4), Rational(6, 8)), Integer(1));
 }
 
 TEST(BinaryDivideOperatorTests, intRatCallOperatorTest) {
+  EXPECT_EQ(*oper(Integer(123083500), Rational(90193108, 13094120)), Rational(402917529755000, 22548277));
 }
 
 TEST(BinaryDivideOperatorTests, ratIntCallOperatorTest) {
+  EXPECT_EQ(*oper(Rational(100000, 10000000), Integer(87539235335)), Rational(1, 8753923533500));
 }
 
 TEST(BinaryDivideOperatorTests, toStringTest) {
+  EXPECT_EQ(oper.toString(), "/");
 }
--- src/math_objects/tests/src/fintamath/math_objects/relations/operators/binary_arithmetic_operators/BinaryMultiplyOperatorTests.cpp
+++ src/math_objects/tests/src/fintamath/math_objects/relations/operators/binary_arithmetic_operators/BinaryMultiplyOperatorTests.cpp
@@ -2,20 +2,38 @@
 
 #include "fintamath/math_objects/relations/operators/arithmetic_operators/binary_arithmetic_operators/BinaryMultiplyOperator.hpp"
 
+using namespace fintamath;
+
+const BinaryMultiplyOperator oper;
+
 TEST(BinaryMultiplyOperatorTests, callOperatorTest) {
+  EXPECT_EQ(*oper({std::make_shared<Integer>(20), std::make_shared<Integer>(10)}).at(0), Integer(200));
+  EXPECT_EQ(*oper({std::make_shared<Rational>(2, 3), std::make_shared<Rational>(3, 4)}).at(0), Rational(1, 2));
+  EXPECT_EQ(*oper({std::make_shared<Integer>(21), std::make_shared<Rational>(3, 4)}).at(0), Rational(63, 4));
+  EXPECT_EQ(*oper({std::make_shared<Rational>(2, 3), std::make_shared<Integer>(10)}).at(0), Rational(20, 3));
+
+  auto s = std::make_shared<Set>();
+  EXPECT_THROW(oper({s, s}), std::invalid_argument);
 }
 
 TEST(BinaryMultiplyOperatorTests, intIntCallOperatorTest) {
+  EXPECT_EQ(*oper(Integer("923749326428379569328756"), Integer("437826432987462398746289734")),
+            Integer("404441872564708456096142978365087208962279873790904"));
 }
 
 TEST(BinaryMultiplyOperatorTests, ratRatCallOperatorTest) {
+  EXPECT_EQ(*oper(Rational(5464, 42424), Rational(854320, 34344265575)), Rational(116700112, 36425528068845));
+  EXPECT_EQ(*oper(Rational(20, 3), Rational(3, 4)), Integer(5));
 }
 
 TEST(BinaryMultiplyOperatorTests, intRatCallOperatorTest) {
+  EXPECT_EQ(*oper(Integer(6584), Rational(9247, 42323)), Rational(60882248, 42323));
 }
 
 TEST(BinaryMultiplyOperatorTests, ratIntCallOperatorTest) {
+  EXPECT_EQ(*oper(Rational(2355852, 343434), Integer(20000)), Rational(7852840000, 57239));
 }
 
 TEST(BinaryMultiplyOperatorTests, toStringTest) {
+  EXPECT_EQ(oper.toString(), "*");
 }
