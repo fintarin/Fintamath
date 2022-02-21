761190d8ba4f1701f0db2b30e2dcfed7ec43fc06
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Implement parser tests

--- src/math_objects/tests/src/fintamath/math_objects/relations/RelationParserTests.cpp
+++ src/math_objects/tests/src/fintamath/math_objects/relations/RelationParserTests.cpp
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
 
-#include "fintamath/math_objects/relations/Relation.hpp"
+#include "fintamath/math_objects/relations/RelationParser.hpp"
+
+using namespace fintamath;
 
 TEST(RelationParserTests, parseTest) {
+  EXPECT_EQ(RelationParser::parse("10"), nullptr);
 }
