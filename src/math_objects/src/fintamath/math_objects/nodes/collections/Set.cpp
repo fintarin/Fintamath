83f25db2c437cb8b0a2e59d264e56e405ab31cdb
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Add dummy Set

---
+++ src/math_objects/src/fintamath/math_objects/nodes/collections/Set.cpp
@@ -0,0 +1,21 @@
+#include "fintamath/math_objects/nodes/collections/Set.hpp"
+
+#include <stdexcept>
+
+using namespace fintamath;
+
+Set::Set(const std::string_view & /*str*/) {
+  throw std::invalid_argument("Set invalid input");
+}
+
+Set::Set(const std::initializer_list<NodePtr> &list) : std::vector<NodePtr>(list) {
+}
+
+std::string Set::toString() const {
+  std::string res = "{";
+  for (const auto &node : *this) {
+    res += node->toString() + ",";
+  }
+  res.erase(res.size() - 1);
+  return res + "}";
+}
