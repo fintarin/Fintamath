cfd5e48fb638afbcc9d019c4936df95e967a48ec
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Implement parser tests

--- src/math_objects/tests/src/fintamath/math_objects/MathObjectParserTests.cpp
+++ src/math_objects/tests/src/fintamath/math_objects/MathObjectParserTests.cpp
@@ -2,5 +2,9 @@
 
 #include "fintamath/math_objects/MathObjectParser.hpp"
 
+using namespace fintamath;
+
 TEST(MathObjectParserTests, parseTest) {
+  EXPECT_EQ(MathObjectParser::parse("1")->toString(), "1");
+  EXPECT_EQ(MathObjectParser::parse("1.1.1"), nullptr);
 }
--- src/math_objects/tests/src/fintamath/math_objects/nodes/terms/TermParserTests.cpp
+++ src/math_objects/tests/src/fintamath/math_objects/nodes/terms/TermParserTests.cpp
@@ -2,5 +2,9 @@
 
 #include "fintamath/math_objects/nodes/terms/TermParser.hpp"
 
+using namespace fintamath;
+
 TEST(TermParserTests, parseTest) {
+  EXPECT_EQ(TermParser::parse("1")->toString(), "1");
+  EXPECT_EQ(TermParser::parse("2.2.2"), nullptr);
 }
