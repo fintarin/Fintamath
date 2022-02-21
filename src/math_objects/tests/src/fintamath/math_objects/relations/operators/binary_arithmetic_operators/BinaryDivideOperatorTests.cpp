9d5c5ca30ddcb9e0be6555112dc8f8846fca5913
fintarin <valukevichv@gmail.com>
2022-02-21 11:48:15 +0300

    Implement BinaryDivideOperator

---
+++ src/math_objects/tests/src/fintamath/math_objects/relations/operators/binary_arithmetic_operators/BinaryDivideOperatorTests.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/binary_arithmetic_operators/BinaryDivideOperator.hpp"
+
+TEST(BinaryDivideOperatorTests, callOperatorTest) {
+}
+
+TEST(BinaryDivideOperatorTests, intIntCallOperatorTest) {
+}
+
+TEST(BinaryDivideOperatorTests, ratRatCallOperatorTest) {
+}
+
+TEST(BinaryDivideOperatorTests, intRatCallOperatorTest) {
+}
+
+TEST(BinaryDivideOperatorTests, ratIntCallOperatorTest) {
+}
+
+TEST(BinaryDivideOperatorTests, toStringTest) {
+}
