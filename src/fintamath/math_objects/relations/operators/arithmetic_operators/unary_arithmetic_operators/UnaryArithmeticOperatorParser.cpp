10b6ea3a19a55cdb96b6ff19f4c798caa8e26726
Семён <81536102+Semen-proger@users.noreply.github.com>
2022-02-22 20:37:57 +0300

    Add relation parsing order

---
+++ src/fintamath/math_objects/relations/operators/arithmetic_operators/unary_arithmetic_operators/UnaryArithmeticOperatorParser.cpp
@@ -0,0 +1,9 @@
+#include "fintamath/math_objects/relations/operators/arithmetic_operators/unary_arithmetic_operators/UnaryArithmeticOperatorParser.hpp"
+
+#include <stdexcept>
+
+namespace fintamath {
+  std::unique_ptr<UnaryArithmeticOperator> UnaryArithmeticOperatorParser::parse(const std::string_view &str) {
+    return nullptr;
+  }
+}
