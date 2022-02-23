6681f92edbac9cd8e2e05bde6b2bd0d389f22760
fintarin <valukevichv@gmail.com>
2022-02-24 01:18:13 +0300

    Remake architecture to make the project a library

---
+++ include/fintamath/math_objects/relations/operators/arithmetic_operators/unary_arithmetic_operators/UnaryArithmeticOperator.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "fintamath/math_objects/relations/operators/Operator.hpp"
+
+namespace fintamath {
+  class UnaryArithmeticOperator : public Operator {
+  public:
+    ~UnaryArithmeticOperator() override = default;
+  };
+}
