dd2076fd838127d943f86d4a4b607d0571b9b5ba
Семён <81536102+Semen-proger@users.noreply.github.com>
2022-02-24 14:27:19 +0300

    Add function for delete opening and closing brackets in string for parsing

--- src/fintamath/math_objects/MathObjectParser.cpp
+++ src/fintamath/math_objects/MathObjectParser.cpp
@@ -6,10 +6,38 @@
 #include "fintamath/math_objects/relations/RelationParser.hpp"
 
 namespace fintamath {
+  std::string_view MathObjectParser::deleteOpenAndCloseBracket(const std::string_view &str) {
+    if (str[0] != '(') {
+      return str;
+    }
+    int leftBracket = 0;
+    int rightBracket = 0;
+    int position = 0;
+    while (position < str.size()) {
+      if (str[position] == '(') {
+        leftBracket++;
+      }
+      if (str[position] == ')') {
+        rightBracket++;
+      }
+      if (leftBracket == rightBracket) {
+        break;
+      }
+      position++;
+    }
+    if (position == str.size()) {
+      throw std::invalid_argument("");
+    }
+    if (position == str.size() - 1) {
+      return str.substr(1, str.size() - 2);
+    }
+    return str;
+  }
   std::unique_ptr<MathObject> MathObjectParser::parse(const std::string_view &str) {
-    if (auto res = RelationParser::parse(str); res) {
+    std::string_view strForParsing = deleteOpenAndCloseBracket(str);
+    if (auto res = RelationParser::parse(strForParsing); res) {
       return res;
     }
-    return NodeParser::parse(str);
+    return NodeParser::parse(strForParsing);
   }
 }
