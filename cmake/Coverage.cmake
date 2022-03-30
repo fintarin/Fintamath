93b7e74403b9ee65946c65eb64e0375315e99773
fintarin <valukevichv@gmail.com>
2022-03-30 13:36:11 +0300

    Refactor CMake

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -1,8 +1,8 @@
 # Enable coverage
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-  option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" OFF)
-  if(ENABLE_COVERAGE)
+  option(${PROJECT_NAME}_enable_coverage "Enable coverage reporting for gcc/clang" OFF)
+  if(${PROJECT_NAME}_enable_coverage)
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage -O0 -g")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage -O0 -g")
   endif()
--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -3,7 +3,7 @@
 include(FetchContent)
 
 # Fetch GTest
-if(BUILD_TESTS)
+if(${PROJECT_NAME}_build_tests)
   message(STATUS "Fetching GTest...")
   FetchContent_Declare(googletest
                        URL https://github.com/google/googletest/archive/609281088cfefc76f9d0ce82e1ff6c30cc3591e5.zip)
