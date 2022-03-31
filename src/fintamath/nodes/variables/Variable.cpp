c79f7d99210677bbbc741bf29130f1fd9d4545f7
Darya <56800428+beklaersiste@users.noreply.github.com>
2022-03-31 18:13:36 +0300

    Add Variable

---
+++ src/fintamath/nodes/variables/Variable.cpp
@@ -0,0 +1,55 @@
+//
+// Created by dashk on 31.03.2022.
+//
+#include "Variable.hpp"
+namespace fintamath {
+  Variable::Variable(const std::string &strVariable) {
+    if (!types::isVariable(strVariable)) {
+      throw std::invalid_argument("Variable invalid input");
+    }
+    name = strVariable;
+  }
+
+  std::string Variable::toString() const {
+    return name;
+  }
+
+  std::unique_ptr<Object> Variable::clone() const {
+    return std::make_unique<Variable>(*this);
+  }
+
+  bool Variable::equals(const Object &rhs) const {
+    return rhs.is<Variable>() && (this->name == rhs.to<Variable>().name);
+  }
+
+  Expression Variable::operator+(const Node &rhs) const {
+    return Expression(this->name + "+" + rhs.toString());
+  }
+
+  Expression Variable::operator-(const Node &rhs) const {
+    return Expression(this->name + "-" + rhs.toString());
+  }
+
+  Expression Variable::operator*(const Node &rhs) const {
+    return Expression(this->name + "*" + rhs.toString());
+  }
+
+  Expression Variable::operator/(const Node &rhs) const {
+    return Expression(this->name + "/" + rhs.toString());
+  }
+
+  bool Variable::operator==(const Node &rhs) const {
+    return this->equals(rhs);
+  }
+
+  bool Variable::operator!=(const Node &rhs) const {
+    return !this->equals(rhs);
+  }
+
+  namespace types {
+    bool isVariable(const std::string &str) {
+      return str.size() == 1 && ((str[0] >= 'A' && str[0] <= 'Z') || (str[0] >= 'a' && str[0] <= 'z')) &&
+             str[0] != 'e' && str[0] != 'i';
+    }
+  }
+}
