58b72b58c8c626d964f218a8acd781801e22866a
fintarin <valukevichv@gmail.com>
2022-04-03 14:25:21 +0300

    Refactor Variable using new interfaces

--- src/fintamath/variables/Variable.cpp
+++ src/fintamath/variables/Variable.cpp
@@ -1,31 +1,31 @@
-//
-// Created by dashk on 31.03.2022.
-//
-#include "Variable.hpp"
+#include "fintamath/variables/Variable.hpp"
+
+#include <regex>
+
 namespace fintamath {
-  Variable::Variable(const std::string &strVariable) {
-    if (!types::isVariable(strVariable)) {
+  Variable::Variable(const std::string &str) {
+    if (!types::isVariable(str)) {
       throw std::invalid_argument("Variable invalid input");
     }
-    name = strVariable;
+    name = str;
   }
 
   std::string Variable::toString() const {
     return name;
   }
 
-  std::unique_ptr<MathObject> Variable::clone() const {
+  std::unique_ptr<MathObjectBase> Variable::clone() const {
     return std::make_unique<Variable>(*this);
   }
 
-  bool Variable::equals(const MathObject &rhs) const {
-    return rhs.is<Variable>() && (this->name == rhs.to<Variable>().name);
+  bool Variable::equals(const Variable &rhs) const {
+    return this->name == rhs.name;
   }
 
   namespace types {
     bool isVariable(const std::string &str) {
-      return str.size() == 1 && ((str[0] >= 'A' && str[0] <= 'Z') || (str[0] >= 'a' && str[0] <= 'z')) &&
-             str[0] != 'e' && str[0] != 'i';
+      std::regex reg(R"([a-df-hj-zA-Z])");
+      return regex_search(str, reg);
     }
   }
 }
--- src/fintamath/variables/Variable.hpp
+++ src/fintamath/variables/Variable.hpp
@@ -6,14 +6,15 @@
 #include "fintamath/expressions/Expression.hpp"
 
 namespace fintamath {
-  class Variable : public MathObject {
+  class Variable : public MathObject<Variable> {
   public:
-    explicit Variable(const std::string &strVariable);
+    explicit Variable(const std::string &str);
+
     std::string toString() const override;
 
-    std::unique_ptr<MathObject> clone() const override;
+    std::unique_ptr<MathObjectBase> clone() const override;
 
-    bool equals(const MathObject &rhs) const override;
+    bool equals(const Variable &rhs) const override;
 
   private:
     std::string name;
