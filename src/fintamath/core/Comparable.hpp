5caa55744433305a73283ef0a92e62d20ece4f0b
fintarin <valukevichv@gmail.com>
2022-04-13 15:27:58 +0300

    Fix Comparable

--- src/fintamath/core/Comparable.hpp
+++ src/fintamath/core/Comparable.hpp
@@ -7,39 +7,63 @@ namespace fintamath {
   public:
     ~Comparable() override = default;
 
-    bool operator<(const Comparable &rhs) const {
-      return less(rhs);
-    }
+    friend bool operator<(const Comparable &lhs, const Comparable &rhs);
 
-    bool operator>(const Comparable &rhs) const {
-      return more(rhs);
-    }
+    friend bool operator>(const Comparable &lhs, const Comparable &rhs);
 
-    bool operator<=(const Comparable &rhs) const {
-      return !more(rhs);
-    }
+    friend bool operator<=(const Comparable &lhs, const Comparable &rhs);
 
-    bool operator>=(const Comparable &rhs) const {
-      return !less(rhs);
-    }
+    friend bool operator>=(const Comparable &lhs, const Comparable &rhs);
 
   protected:
-    virtual bool less(const Comparable &rhs) const = 0;
+    virtual bool lessAbstract(const Comparable &rhs) const = 0;
 
-    virtual bool more(const Comparable &rhs) const = 0;
+    virtual bool moreAbstract(const Comparable &rhs) const = 0;
   };
+
+  inline bool operator<(const Comparable &lhs, const Comparable &rhs) {
+    return lhs.lessAbstract(rhs);
+  }
+
+  inline bool operator>(const Comparable &lhs, const Comparable &rhs) {
+    return lhs.moreAbstract(rhs);
+  }
+
+  inline bool operator<=(const Comparable &lhs, const Comparable &rhs) {
+    return !lhs.moreAbstract(rhs);
+  }
+
+  inline bool operator>=(const Comparable &lhs, const Comparable &rhs) {
+    return !lhs.lessAbstract(rhs);
+  }
 
   template <typename Derived>
   class ComparableImpl : virtual public Comparable, virtual public MathObjectImpl<Derived> {
   public:
     ~ComparableImpl() override = default;
 
+    bool operator<(const Derived &rhs) const {
+      return less(rhs);
+    }
+
+    bool operator>(const Derived &rhs) const {
+      return more(rhs);
+    }
+
+    bool operator<=(const Derived &rhs) const {
+      return !more(rhs);
+    }
+
+    bool operator>=(const Derived &rhs) const {
+      return !less(rhs);
+    }
+
   protected:
     virtual bool less(const Derived &rhs) const = 0;
 
     virtual bool more(const Derived &rhs) const = 0;
 
-    bool less(const Comparable &rhs) const final {
+    bool lessAbstract(const Comparable &rhs) const final {
       if (rhs.is<Derived>()) {
         return less(rhs.to<Derived>());
       }
@@ -52,7 +76,7 @@ namespace fintamath {
       throw std::invalid_argument("Cannot be compared");
     }
 
-    bool more(const Comparable &rhs) const final {
+    bool moreAbstract(const Comparable &rhs) const final {
       if (rhs.is<Derived>()) {
         return more(rhs.to<Derived>());
       }
@@ -67,56 +91,56 @@ namespace fintamath {
   };
 
   template <typename LhsType, typename RhsType,
-            typename = std::enable_if_t<std::is_base_of_v<ComparableImpl<LhsType>, LhsType> &&
+            typename = std::enable_if_t<std::is_base_of_v<Comparable, LhsType> &&
                                         std::is_convertible_v<RhsType, LhsType> && !std::is_same_v<LhsType, RhsType>>>
   bool operator<(const LhsType &lhs, const RhsType &rhs) {
     return lhs < LhsType(rhs);
   }
 
   template <typename RhsType, typename LhsType,
-            typename = std::enable_if_t<std::is_base_of_v<ComparableImpl<RhsType>, RhsType> &&
+            typename = std::enable_if_t<std::is_base_of_v<Comparable, RhsType> &&
                                         std::is_convertible_v<LhsType, RhsType> && !std::is_same_v<LhsType, RhsType>>>
   bool operator<(const LhsType &lhs, const RhsType &rhs) {
     return RhsType(lhs) < rhs;
   }
 
   template <typename LhsType, typename RhsType,
-            typename = std::enable_if_t<std::is_base_of_v<ComparableImpl<LhsType>, LhsType> &&
+            typename = std::enable_if_t<std::is_base_of_v<Comparable, LhsType> &&
                                         std::is_convertible_v<RhsType, LhsType> && !std::is_same_v<LhsType, RhsType>>>
   bool operator>(const LhsType &lhs, const RhsType &rhs) {
     return lhs > LhsType(rhs);
   }
 
   template <typename RhsType, typename LhsType,
-            typename = std::enable_if_t<std::is_base_of_v<ComparableImpl<RhsType>, RhsType> &&
+            typename = std::enable_if_t<std::is_base_of_v<Comparable, RhsType> &&
                                         std::is_convertible_v<LhsType, RhsType> && !std::is_same_v<LhsType, RhsType>>>
   bool operator>(const LhsType &lhs, const RhsType &rhs) {
     return RhsType(lhs) > rhs;
   }
 
   template <typename LhsType, typename RhsType,
-            typename = std::enable_if_t<std::is_base_of_v<ComparableImpl<LhsType>, LhsType> &&
+            typename = std::enable_if_t<std::is_base_of_v<Comparable, LhsType> &&
                                         std::is_convertible_v<RhsType, LhsType> && !std::is_same_v<LhsType, RhsType>>>
   bool operator<=(const LhsType &lhs, const RhsType &rhs) {
     return lhs <= LhsType(rhs);
   }
 
   template <typename RhsType, typename LhsType,
-            typename = std::enable_if_t<std::is_base_of_v<ComparableImpl<RhsType>, RhsType> &&
+            typename = std::enable_if_t<std::is_base_of_v<Comparable, RhsType> &&
                                         std::is_convertible_v<LhsType, RhsType> && !std::is_same_v<LhsType, RhsType>>>
   bool operator<=(const LhsType &lhs, const RhsType &rhs) {
     return RhsType(lhs) <= rhs;
   }
 
   template <typename LhsType, typename RhsType,
-            typename = std::enable_if_t<std::is_base_of_v<ComparableImpl<LhsType>, LhsType> &&
+            typename = std::enable_if_t<std::is_base_of_v<Comparable, LhsType> &&
                                         std::is_convertible_v<RhsType, LhsType> && !std::is_same_v<LhsType, RhsType>>>
   bool operator>=(const LhsType &lhs, const RhsType &rhs) {
     return lhs >= LhsType(rhs);
   }
 
   template <typename RhsType, typename LhsType,
-            typename = std::enable_if_t<std::is_base_of_v<ComparableImpl<RhsType>, RhsType> &&
+            typename = std::enable_if_t<std::is_base_of_v<Comparable, RhsType> &&
                                         std::is_convertible_v<LhsType, RhsType> && !std::is_same_v<LhsType, RhsType>>>
   bool operator>=(const LhsType &lhs, const RhsType &rhs) {
     return RhsType(lhs) >= rhs;
