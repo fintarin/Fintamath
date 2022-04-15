2cdc11ec7794ab4c0bd01d7eaa423dafc1e421d2
fintarin <valukevichv@gmail.com>
2022-04-15 12:42:44 +0300

    Implement ComparableTests

---
+++ tests/src/core/ComparableTests.cpp
@@ -0,0 +1,130 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/core/Comparable.hpp"
+
+#include "fintamath/numbers/Integer.hpp"
+#include "fintamath/numbers/Rational.hpp"
+
+using namespace fintamath;
+
+namespace fintamath::tests {
+  class Test : public ComparableImpl<Test> {
+  public:
+    std::string toString() const override {
+      return {};
+    }
+
+  protected:
+    bool equals(const Test &rhs) const override {
+      return true;
+    }
+
+    bool less(const Test &rhs) const override {
+      return true;
+    }
+
+    bool more(const Test &rhs) const override {
+      return true;
+    }
+  };
+}
+
+TEST(ComparableTests, lessTest) {
+  ComparablePtr m1 = std::make_unique<Integer>(1);
+  ComparablePtr m2 = std::make_unique<Rational>(2);
+  ComparablePtr m3 = std::make_unique<Rational>(1, 2);
+
+  EXPECT_FALSE(*m1 < *m1);
+  EXPECT_FALSE(*m2 < *m2);
+  EXPECT_TRUE(*m1 < *m2);
+  EXPECT_FALSE(*m2 < *m1);
+
+  EXPECT_FALSE(*m1 < *m3);
+  EXPECT_FALSE(*m2 < *m3);
+  EXPECT_TRUE(*m3 < *m1);
+  EXPECT_TRUE(*m3 < *m2);
+
+  EXPECT_THROW(*m1 < tests::Test(), std::invalid_argument);
+  EXPECT_THROW(tests::Test() < *m1, std::invalid_argument);
+
+  EXPECT_TRUE(Integer() < 1);
+  EXPECT_TRUE(-1 < Integer());
+
+  EXPECT_FALSE(Integer() < 0);
+  EXPECT_FALSE(1 < Integer());
+}
+
+TEST(ComparableTests, moreTest) {
+  ComparablePtr m1 = std::make_unique<Integer>(1);
+  ComparablePtr m2 = std::make_unique<Rational>(2);
+  ComparablePtr m3 = std::make_unique<Rational>(1, 2);
+
+  EXPECT_FALSE(*m1 > *m1);
+  EXPECT_FALSE(*m2 > *m2);
+  EXPECT_FALSE(*m1 > *m2);
+  EXPECT_TRUE(*m2 > *m1);
+
+  EXPECT_TRUE(*m1 > *m3);
+  EXPECT_TRUE(*m2 > *m3);
+  EXPECT_FALSE(*m3 > *m1);
+  EXPECT_FALSE(*m3 > *m2);
+
+  EXPECT_THROW(*m1 > tests::Test(), std::invalid_argument);
+  EXPECT_THROW(tests::Test() > *m1, std::invalid_argument);
+
+  EXPECT_FALSE(Integer() > 1);
+  EXPECT_FALSE(-1 > Integer());
+
+  EXPECT_FALSE(Integer() > 0);
+  EXPECT_TRUE(1 > Integer());
+}
+
+TEST(ComparableTests, lessEqualsTest) {
+  ComparablePtr m1 = std::make_unique<Integer>(1);
+  ComparablePtr m2 = std::make_unique<Rational>(2);
+  ComparablePtr m3 = std::make_unique<Rational>(1, 2);
+
+  EXPECT_TRUE(*m1 <= *m1);
+  EXPECT_TRUE(*m2 <= *m2);
+  EXPECT_TRUE(*m1 <= *m2);
+  EXPECT_FALSE(*m2 <= *m1);
+
+  EXPECT_FALSE(*m1 <= *m3);
+  EXPECT_FALSE(*m2 <= *m3);
+  EXPECT_TRUE(*m3 <= *m1);
+  EXPECT_TRUE(*m3 <= *m2);
+
+  EXPECT_THROW(*m1 <= tests::Test(), std::invalid_argument);
+  EXPECT_THROW(tests::Test() <= *m1, std::invalid_argument);
+
+  EXPECT_TRUE(Integer() <= 1);
+  EXPECT_TRUE(-1 <= Integer());
+
+  EXPECT_TRUE(Integer() <= 0);
+  EXPECT_FALSE(1 <= Integer());
+}
+
+TEST(ComparableTests, moreEqualsTest) {
+  ComparablePtr m1 = std::make_unique<Integer>(1);
+  ComparablePtr m2 = std::make_unique<Rational>(2);
+  ComparablePtr m3 = std::make_unique<Rational>(1, 2);
+
+  EXPECT_TRUE(*m1 >= *m1);
+  EXPECT_TRUE(*m2 >= *m2);
+  EXPECT_FALSE(*m1 >= *m2);
+  EXPECT_TRUE(*m2 >= *m1);
+
+  EXPECT_TRUE(*m1 >= *m3);
+  EXPECT_TRUE(*m2 >= *m3);
+  EXPECT_FALSE(*m3 >= *m1);
+  EXPECT_FALSE(*m3 >= *m2);
+
+  EXPECT_THROW(*m1 >= tests::Test(), std::invalid_argument);
+  EXPECT_THROW(tests::Test() >= *m1, std::invalid_argument);
+
+  EXPECT_FALSE(Integer() >= 1);
+  EXPECT_FALSE(-1 >= Integer());
+
+  EXPECT_TRUE(Integer() >= 0);
+  EXPECT_TRUE(1 >= Integer());
+}
