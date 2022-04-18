8790b832ee2c3dfd70967145e58eb333e2078501
Семён <81536102+Semen-proger@users.noreply.github.com>
2022-04-18 17:35:17 +0300

    Add Expression Tests

--- tests/src/expressions/ExpressionTests.cpp
+++ tests/src/expressions/ExpressionTests.cpp
@@ -7,15 +7,54 @@
 
 using namespace fintamath;
 
-TEST(ExpressionTests, expressionPositiveTests) {
-  std::ifstream testsIn(RESOURCES_DIR "positive_tests.txt");
-  for (std::string input, output; getline(testsIn, input) && getline(testsIn, output);) {
-    std::cout << input << '\n' << output << "\n\n";
-    EXPECT_EQ(Expression(input).simplify().toString(), output);
-    getline(testsIn, input);
-  }
+TEST(ExpressionTests, toStringTest){
+
 }
 
-TEST(ExpressionTests, parserTest){
-  auto a = Expression("log( 1 + 2 , a)");
+TEST(ExpressionTests, constructorTest){
+  EXPECT_EQ(Expression().toString(), "");
+}
+
+TEST(ExpressionTests, moveTest){
+  auto a = Expression("1+2-3");
+  EXPECT_EQ(a.toString(),"0");
+  auto b = std::move(a);
+  EXPECT_TRUE(b.toString() == "0" && a.toString() == "");
+  a = std::move(b);
+  EXPECT_TRUE(a.toString() == "0" && b.toString() == "");
+
+}
+
+TEST(ExpressionTests, copyTest){
+  auto a = Expression("1+2");
+  auto b = a;
+  EXPECT_TRUE(a == b && &a != &b);
+}
+
+TEST(ExpressionTests, simplifyTest){
+    EXPECT_EQ(Expression("2").toString(), "2");
+    EXPECT_EQ(Expression("2 + 2").toString(), "4");
+    EXPECT_EQ(Expression("2 * 2").toString(), "4");
+    EXPECT_EQ(Expression("2 ^ 2").toString(), "4");
+    EXPECT_EQ(Expression("2 + 2 * 2").toString(), "6");
+    EXPECT_EQ(Expression("(2 + 2) * 2").toString(), "8");
+    EXPECT_EQ(Expression("(2 + 2) / (2 ^ 2 - 2) * 2").toString(), "4");
+    EXPECT_EQ(Expression("((((2))))").toString(), "2");
+    EXPECT_EQ(Expression("(((((2 + 2) + 2) + 2) + 2) + 2)").toString(), "12");
+    EXPECT_EQ(Expression("(((2 ^ 2)^2))").toString(), "16");
+    EXPECT_EQ(Expression("2^2^2^2").toString(), "65536");
+    EXPECT_EQ(Expression("(2-2)").toString(), "0");
+    EXPECT_EQ(Expression("0^1").toString(), "0");
+    EXPECT_EQ(Expression("2^0").toString(), "1");
+    EXPECT_EQ(Expression("0/3").toString(), "0");
+    EXPECT_EQ(Expression("1/3").toString(), "1/3");
+    EXPECT_EQ(Expression("-2+3").toString(), "1");
+
+
+    EXPECT_EQ(Expression("-(-(-(-(-(-(-(-a)))))))").toString(), "a");
+    EXPECT_EQ(Expression("a+a").toString(), "2*a");
+    EXPECT_EQ(Expression("a-a").toString(), "0");
+    EXPECT_EQ(Expression("(a+b)-b").toString(), "a");
+
+    EXPECT_EQ(Expression("(a+b)*(a+b)+a*b*c-c*a*b+b*a").toString(), "b*b+a*a+3*a*b");
 }
