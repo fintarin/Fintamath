635a18ce9618def2a639fb274784e03ca4e5b021
Darya <56800428+beklaersiste@users.noreply.github.com>
2022-04-30 14:40:07 +0300

    Add DoubleFactorial

---
+++ src/fintamath/functions/DoubleFactorial.hpp
@@ -0,0 +1,19 @@
+#include "Function.hpp"
+
+namespace fintamath {
+  class DoubleFactorial: public FunctionImpl<DoubleFactorial> {
+  public:
+
+    explicit DoubleFactorial() = default;
+
+    std::string toString() const override;
+
+    MathObjectPtr operator()(const MathObject &rhs) const;
+
+  protected:
+
+    bool equals(const DoubleFactorial& rhs) const override;
+
+    MathObjectPtr call(const std::vector<std::reference_wrapper<const MathObject>> &argsVect) const override;
+  };
+}
