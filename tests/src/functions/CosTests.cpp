8a71a388f1522d1cd806aaceaa1ee51c7aa82b84
Darya <56800428+beklaersiste@users.noreply.github.com>
2022-04-30 13:31:29 +0300

    Add Cos

---
+++ tests/src/functions/CosTests.cpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+#include <fintamath/operators/Add.hpp>
+#include <fintamath/variables/Variable.hpp>
+#include <fintamath/constants/Constant.hpp>
+
+#include <fintamath/functions/Cos.hpp>
+
+using namespace fintamath;
+
+TEST(CosTests, toStringTest) {
+  EXPECT_EQ(Cos().toString(), "cos");
+}
+
+TEST(CosTests, callTest){
+  EXPECT_EQ(Cos()(Integer(0))->to<Rational>().toString(5), "1");
+  EXPECT_EQ(Cos()(Integer(10))->to<Rational>().toString(10), "-0.8390715291");
+  EXPECT_EQ(Cos()(Integer(5))->to<Rational>().toString(36), "0.283662185463226264466639171513557308");
+  EXPECT_EQ(Cos()(Rational(1, 10))->to<Rational>().toString(10), "0.9950041653");
+  EXPECT_EQ(Cos()(Constant("pi").toRational(10) / 2)->to<Rational>().toString(5), "-0");
+
+  EXPECT_ANY_THROW(Cos()(Variable("a")));
+
+  std::unique_ptr<Function> f = std::make_unique<Cos>();
+  EXPECT_EQ((*f)(Integer(10))->to<Rational>().toString(10), "-0.8390715291");
+  EXPECT_ANY_THROW((*f)());
+  EXPECT_ANY_THROW((*f)(Integer(1), Integer(1), Integer(1)));
+}
+
+TEST(CosTests, equalsTest) {
+  EXPECT_TRUE(Cos() == Cos());
+  EXPECT_FALSE(Cos() == Add());
+}
