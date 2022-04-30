8835de5cbe6494e7c85b90e7acc304ea9feae848
Darya <56800428+beklaersiste@users.noreply.github.com>
2022-04-30 11:25:00 +0300

    Add Ln

---
+++ tests/src/functions/LnTests.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+#include <fintamath/operators/Add.hpp>
+#include <fintamath/variables/Variable.hpp>
+#include <fintamath/constants/Constant.hpp>
+
+#include <fintamath/functions/Ln.hpp>
+
+using namespace fintamath;
+
+TEST(LnTests, toStringTest) {
+  EXPECT_EQ(Ln().toString(), "ln");
+}
+
+TEST(LnTests, callTest){
+  EXPECT_EQ(Ln()(Integer(1))->to<Rational>().toString(5), "0");
+  EXPECT_EQ(Ln()(Integer(10))->to<Rational>().toString(10), "2.302585093");
+  EXPECT_EQ(Ln()(Integer(5))->to<Rational>().toString(36), "1.609437912434100374600759333226187489");
+  EXPECT_EQ(Ln()(Rational(1, 10))->to<Rational>().toString(10), "-2.302585093");
+  EXPECT_EQ(Ln()(Constant("e").toRational(10))->to<Rational>().toString(5), "1");
+
+  EXPECT_ANY_THROW(Ln()(Variable("a")));
+  EXPECT_ANY_THROW(Ln()(Integer(-10)));
+
+  std::unique_ptr<Function> f = std::make_unique<Ln>();
+  EXPECT_EQ((*f)(Integer(10))->to<Rational>().toString(10), "2.302585093");
+  EXPECT_ANY_THROW((*f)());
+  EXPECT_ANY_THROW((*f)(Integer(1), Integer(1), Integer(1)));
+}
+
+TEST(LnTests, equalsTest) {
+  EXPECT_TRUE(Ln() == Ln());
+  EXPECT_FALSE(Ln() == Add());
+}
