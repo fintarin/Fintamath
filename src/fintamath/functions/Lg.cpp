f00e090ba97c8f10fe1321b05c99e443fe39f518
Darya <56800428+beklaersiste@users.noreply.github.com>
2022-04-30 11:53:11 +0300

    Add Lg

---
+++ src/fintamath/functions/Lg.cpp
@@ -0,0 +1,30 @@
+#include "Lg.hpp"
+#include "NamespaceFunctions.hpp"
+
+namespace fintamath {
+
+  std::string Lg::toString() const {
+    return "lg";
+  }
+
+  MathObjectPtr Lg::operator()(const MathObject &rhs) const {
+    const int64_t defaultPrecision = 36;
+    if (!rhs.instanceOf<Arithmetic>()) {
+      throw std::invalid_argument("Types must be Arithmetic");
+    }
+    auto newRhs = meta::convertRhsToLhsType(Rational(), rhs);
+    return std::make_unique<Rational>(functions::lg(newRhs->to<Rational>(), defaultPrecision));
+  }
+
+  bool Lg::equals(const Lg & /*rhs*/) const {
+    return true;
+  }
+
+  MathObjectPtr Lg::call(const std::vector<std::reference_wrapper<const MathObject>> &argsVect) const {
+    if (argsVect.size() != 1) {
+      throw std::invalid_argument("The number of arguments must be 1");
+    }
+    return Lg::operator()(argsVect.at(0).get());
+  }
+
+}
