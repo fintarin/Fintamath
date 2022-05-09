23220584b7256a2e4105e6a125da8cfcd8bd5424
ProtasSemyon <sem1235463782@gmail.com>
2022-05-09 20:26:00 +0300

    Fix Functions tests

--- tests/src/functions/trigonometry/CotTests.cpp
+++ tests/src/functions/trigonometry/CotTests.cpp
@@ -12,7 +12,7 @@ TEST(CotTests, toStringTest) {
 }
 
 TEST(CotTests, callTest) {
-  EXPECT_EQ(Cot()(Constant("pi").toRational(10) / 2)->toString(), "-0");
+  EXPECT_EQ(Cot()(Constant("pi").toRational(10) / 2)->toString(), "0");
   EXPECT_EQ(Cot()(Integer(10))->toString(), "1.542351045356920048277469355682429311");
   EXPECT_EQ(Cot()(Integer(-10))->toString(), "-1.542351045356920048277469355682429311");
   EXPECT_EQ(Cot()(Integer(5))->toString(), "-0.295812915532745540427767168082485286");
