cd12cefe320898016c48823c2eff820ee6b1fbc3
fintarin <valukevichv@gmail.com>
2022-10-09 01:52:02 +0300

    Update gtests

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -5,11 +5,16 @@ include(FetchContent)
 # Fetch GTest
 if(${PROJECT_NAME}_build_tests)
   message(STATUS "Fetching GTest...")
-  FetchContent_Declare(googletest
-                       URL https://github.com/google/googletest/archive/609281088cfefc76f9d0ce82e1ff6c30cc3591e5.zip)
+
+  FetchContent_Declare(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG release-1.12.1)
+
   set(gtest_force_shared_crt
       ON
       CACHE BOOL "" FORCE)
   FetchContent_MakeAvailable(googletest)
+
   message(STATUS "GTest fetched")
 endif()
