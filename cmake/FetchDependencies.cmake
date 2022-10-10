9f8faa01dbbf14d582d87378d55a98bff0b3bb3a
fintarin <valukevichv@gmail.com>
2022-10-10 13:28:47 +0300

    Add Eigen3

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -5,6 +5,20 @@ include(FetchContent)
 # Find GMP
 find_package(GMP REQUIRED)
 
+# Fetch Eigen3
+if(NOT TARGET Eigen3::Eigen)
+  FetchContent_Declare(
+    eigen3
+    GIT_REPOSITORY https://gitlab.com/libeigen/eigen
+    GIT_TAG 3.4.0)
+
+  set(EIGEN_BUILD_DOC OFF)
+  set(BUILD_TESTING OFF)
+  set(EIGEN_BUILD_PKGCONFIG OFF)
+
+  FetchContent_MakeAvailable(eigen3)
+endif()
+
 # Fetch GTest
 if(${PROJECT_NAME}_build_tests)
   message(STATUS "Fetching GTest...")
