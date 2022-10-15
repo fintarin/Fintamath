c391acdbbb54649fecfca462276f845d3c9b4d5a
fintarin <valukevichv@gmail.com>
2022-10-15 23:46:50 +0300

    Remove Eigen3

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -2,23 +2,6 @@
 
 include(FetchContent)
 
-# Fetch Eigen3
-if(NOT TARGET Eigen3::Eigen)
-  FetchContent_Declare(
-    Eigen
-    GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
-    GIT_TAG master
-    GIT_SHALLOW TRUE
-    GIT_PROGRESS TRUE)
-
-  set(EIGEN_BUILD_DOC OFF)
-  set(BUILD_TESTING OFF)
-  set(EIGEN_BUILD_PKGCONFIG OFF)
-  set(OFF)
-  
-  FetchContent_MakeAvailable(Eigen)
-endif()
-
 # Fetch GTest
 if(${PROJECT_NAME}_build_tests)
   message(STATUS "Fetching GTest...")
