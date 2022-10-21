1ba4aeb3b918d65b7de405e9706e02f16c69a6a1
fintarin <valukevichv@gmail.com>
2022-10-21 17:44:29 +0300

    Refactoring

--- src/fintamath/literals/Variable.cpp
+++ src/fintamath/literals/Variable.cpp
@@ -6,22 +6,22 @@ namespace fintamath {
   bool isVariable(const std::string &str);
 
   Variable::Variable(const std::string &str) {
-    parse(str);
-  }
-
-  std::string Variable::toString() const {
-    return name;
-  }
-
-  void Variable::parse(const std::string &str) {
     if (!isVariable(str)) {
       throw std::invalid_argument("Variable invalid input");
     }
     name = str;
   }
 
+  std::string Variable::toString() const {
+    return name;
+  }
+
   bool isVariable(const std::string &str) {
-    std::regex reg(R"(^[a-df-hj-zA-Z]$)");
+    if (str.size() != 1) {
+      return false;
+    }
+
+    const std::regex reg(R"(^[a-df-hj-zA-Z]$)");
     return regex_search(str, reg);
   }
 }
