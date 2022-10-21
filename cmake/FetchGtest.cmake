c0ed14851fee0c421635bb1bdcac5620cca3ef6a
fintarin <valukevichv@gmail.com>
2022-10-21 12:25:59 +0300

    Refactor CMake

--- cmake/FetchGtest.cmake
+++ cmake/FetchGtest.cmake
@@ -2,18 +2,16 @@
 
 include(FetchContent)
 
-if(${PROJECT_NAME}_build_tests)
-  message(STATUS "Fetching GTest...")
+message(STATUS "Fetching GTest...")
 
-  FetchContent_Declare(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG release-1.12.1)
+FetchContent_Declare(
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG release-1.12.1)
 
-  set(gtest_force_shared_crt
-      ON
-      CACHE BOOL "" FORCE)
-  FetchContent_MakeAvailable(googletest)
+set(gtest_force_shared_crt
+    ON
+    CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
 
-  message(STATUS "GTest fetched")
-endif()
+message(STATUS "GTest fetched")
