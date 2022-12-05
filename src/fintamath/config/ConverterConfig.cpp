bbec577faf4bf5d9967b80aa93fb6fe2e21652cd
taramin <dtaramin@mail.ru>
2022-12-05 16:30:14 +0300

    Update ConverterConfig

--- src/fintamath/config/ConverterConfig.cpp
+++ src/fintamath/config/ConverterConfig.cpp
@@ -3,6 +3,7 @@
 #include "fintamath/helpers/Converter.hpp"
 #include "fintamath/numbers/Integer.hpp"
 #include "fintamath/numbers/Rational.hpp"
+#include "fintamath/numbers/Real.hpp"
 
 using namespace fintamath::helpers;
 
@@ -14,10 +15,18 @@ namespace fintamath::config {
   ConverterConfig::ConverterConfig() {
     Converter::add<Integer, Integer>(
         [](const Integer &value, const Integer & /*type*/) { return std::make_unique<Integer>(value); });
+
     Converter::add<Rational, Rational>(
         [](const Rational &value, const Rational & /*type*/) { return std::make_unique<Rational>(value); });
     Converter::add<Integer, Rational>(
         [](const Integer &value, const Rational & /*type*/) { return std::make_unique<Rational>(value); });
+
+    Converter::add<Real, Real>(
+      [](const Real &value, const Real & /*type*/) { return std::make_unique<Real>(value); });
+    Converter::add<Integer, Real>(
+        [](const Integer &value, const Real & /*type*/) { return std::make_unique<Real>(value); });
+    Converter::add<Rational, Real>(
+        [](const Rational &value, const Real & /*type*/) { return std::make_unique<Real>(value); });
   }
 
   static const ConverterConfig config;
--- tests/src/meta/ConverterTests.cpp
+++ tests/src/meta/ConverterTests.cpp
@@ -3,12 +3,21 @@
 #include "fintamath/helpers/Converter.hpp"
 
 #include "fintamath/numbers/Rational.hpp"
+#include "fintamath/numbers/Real.hpp"
 
 using namespace fintamath;
 using namespace fintamath::helpers;
 
 TEST(ConverterTests, convertTest) {
-  EXPECT_TRUE(Converter::convert(Integer(), Rational())->is<Rational>());
+  EXPECT_TRUE(Converter::convert(Integer(), Integer())->is<Integer>());
 
+  EXPECT_TRUE(Converter::convert(Rational(), Rational())->is<Rational>());
+  EXPECT_TRUE(Converter::convert(Integer(), Rational())->is<Rational>());
   EXPECT_FALSE(Converter::convert(Rational(), Integer()));
+
+  EXPECT_TRUE(Converter::convert(Real(), Real())->is<Real>());
+  EXPECT_TRUE(Converter::convert(Integer(), Real())->is<Real>());
+  EXPECT_FALSE(Converter::convert(Real(), Integer()));
+  EXPECT_TRUE(Converter::convert(Rational(), Real())->is<Real>());
+  EXPECT_FALSE(Converter::convert(Real(), Rational()));
 }
