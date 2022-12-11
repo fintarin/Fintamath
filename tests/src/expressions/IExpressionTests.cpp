be13f78017244e7037e908c59a97986cf6d1ed77
taramin <dtaramin@mail.ru>
2022-12-11 13:25:07 +0300

    Add expression tests

---
+++ tests/src/expressions/IExpressionTests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/expressions/IExpression.hpp"
+
+#include <fstream>
+
+using namespace fintamath;
+
+// TODO add tests for all public functions
+
+TEST(IExpressionTests, toStringTest) {
+}
---
+++ tests/src/expressions/MulExpressionTests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/expressions/MulExpression.hpp"
+
+#include <fstream>
+
+using namespace fintamath;
+
+// TODO add tests for all public functions
+
+TEST(MulExpressionTests, toStringTest) {
+}
