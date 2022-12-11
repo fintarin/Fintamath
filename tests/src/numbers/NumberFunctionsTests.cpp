16278ed6673ba0e3e788019fced1ab83dddc20df
Darya <56800428+beklaersiste@users.noreply.github.com>
2022-12-11 17:53:01 +0300

    NumberFunctionsTests

--- tests/src/numbers/NumberFunctionsTests.cpp
+++ tests/src/numbers/NumberFunctionsTests.cpp
@@ -1,11 +1,20 @@
 #include <gtest/gtest.h>
 
 #include "fintamath/numbers/NumberFunctions.hpp"
-
-#include "fintamath/exceptions/UndefinedFunctionException.hpp"
+#include "fintamath/numbers/Integer.hpp"
+#include "fintamath/numbers/Rational.hpp"
+#include "fintamath/numbers/Real.hpp"
 
 using namespace fintamath;
 
 TEST(NumberFunctionsTests, absTest) {
-  // TODO
+  EXPECT_EQ(abs(Integer("-210")).toString(), "210");
+  EXPECT_EQ(abs(Integer("4545628562")).toString(), "4545628562");
+
+  EXPECT_EQ(abs(Rational(-1, 100)).toString(), "1/100");
+  EXPECT_EQ(abs(Rational(1, -100)).toString(), "1/100");
+  EXPECT_EQ(abs(Rational(10, 1000)).toString(), "1/100");
+
+  EXPECT_EQ(abs(Real("8465132.321651651")).toString(), "8465132.321651651");
+  EXPECT_EQ(abs(Real("-98465136846516354684651.351")).toString(), "98465136846516354684651.351");
 }
