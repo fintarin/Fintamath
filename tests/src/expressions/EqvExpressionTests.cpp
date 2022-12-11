6698b72a12d3c812eb26e77179451203e241afdc
taramin <dtaramin@mail.ru>
2022-12-11 22:07:48 +0300

    Add EqvExpressionTests

---
+++ tests/src/expressions/EqvExpressionTests.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/expressions/EqvExpression.hpp"
+
+#include <fstream>
+
+using namespace fintamath;
+
+TEST(EqvExpressionTests, toStringTest) {
+  // EXPECT_EQ(Expression("a+5<3").toString(), "a+5<3");
+}
