c246ae67835266e2901c2accc9fdd9732ffd4d9e
fintarin <valukevichv@gmail.com>
2023-01-05 13:46:56 +0300

    Refactor cmake

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -4,7 +4,6 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clan
   option(${PROJECT_NAME}_enable_sanitizers "Enable sanitizers" OFF)
   if(${PROJECT_NAME}_enable_sanitizers)
     set(sanitizers "address,undefined")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=${sanitizers}")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=${sanitizers}")
   endif()
 endif()
