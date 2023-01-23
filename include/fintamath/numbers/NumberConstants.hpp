f88d0fb52298673115b0a9bfc7cb8de4b1a6096f
fintarin <valukevichv@gmail.com>
2023-01-23 15:06:55 +0300

    Add more Number constants

--- include/fintamath/numbers/NumberConstants.hpp
+++ include/fintamath/numbers/NumberConstants.hpp
@@ -13,6 +13,12 @@ extern const Integer TWO;
 
 extern const Integer THREE;
 
+extern const Integer NEG_ONE;
+
+extern const Integer NEG_TWO;
+
+extern const Integer NEG_THREE;
+
 extern const Real E_NUM;
 
 extern const Real PI_NUM;
--- src/fintamath/numbers/NumberConstants.cpp
+++ src/fintamath/numbers/NumberConstants.cpp
@@ -12,6 +12,12 @@ const Integer TWO = 2;
 
 const Integer THREE = 3;
 
+const Integer NEG_ONE = -1;
+
+const Integer NEG_TWO = -2;
+
+const Integer NEG_THREE = -3;
+
 const Real E_NUM = RealImpl(default_ops::get_constant_e<RealImpl::Backend::backend_type>());
 
 const Real PI_NUM = RealImpl(default_ops::get_constant_pi<RealImpl::Backend::backend_type>());
