d829c8a31dec8b0e64453eae760ecb0cfbb0c981
fintarin <valukevichv@gmail.com>
2023-01-29 15:50:16 +0300

    Fix NumberFunctions

--- include/fintamath/numbers/NumberFunctions.hpp
+++ include/fintamath/numbers/NumberFunctions.hpp
@@ -18,28 +18,25 @@ inline NumberPtr abs(const INumber &rhs) {
 template <typename Lhs, typename Rhs,
           typename = std::enable_if_t<std::is_base_of_v<INumber, Lhs> && std::is_base_of_v<INumber, Rhs> &&
                                       !std::is_same_v<Real, Lhs> && !std::is_same_v<Real, Rhs>>>
-NumberPtr pow(const Lhs &inLhs, const Rhs &inRhs) {
-  auto lhsPtr = inLhs.simplify();
-  auto rhsPtr = inRhs.simplify();
+NumberPtr pow(const Lhs &lhs, const Rhs &rhs) {
+  auto lhsSimpl = cast<INumber>(lhs.simplify());
+  auto rhsSimpl = cast<INumber>(rhs.simplify());
 
-  const auto *lhs = cast<INumber>(lhsPtr.get());
-  const auto *rhs = cast<INumber>(rhsPtr.get());
-
-  if (const auto *rhsInt = cast<Integer>(rhs)) {
-    if (*rhsInt < ZERO) {
-      return pow(*(ONE / *lhs), -(*rhsInt));
-    }
-
-    return pow(*lhs, *rhsInt);
+  if (const auto *rhsInt = cast<Integer>(rhsSimpl.get())) {
+    return pow(*lhsSimpl, *rhsInt);
   }
 
-  return cast<INumber>(pow(convert<Real>(*lhs), convert<Real>(*rhs)).simplify());
+  return cast<INumber>(pow(convert<Real>(*lhsSimpl), convert<Real>(*rhsSimpl)).simplify());
 }
 
 template <typename Lhs, typename = std::enable_if_t<std::is_base_of_v<INumber, Lhs> && !std::is_same_v<Real, Lhs>>>
 NumberPtr pow(const Lhs &lhs, Integer rhs) {
   if (lhs == ZERO && rhs == ZERO) {
     throw UndefinedBinaryOpearatorException("^", lhs.toString(), rhs.toString());
+  }
+
+  if (rhs < ZERO) {
+    return pow(*(ONE / cast<INumber>(lhs)), -rhs);
   }
 
   NumberPtr res = std::make_unique<Integer>(ONE);
