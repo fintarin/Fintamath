e140c59a362c887704c9cbded545125cca491626
Semen <sem1235463782@gmail.com>
2023-03-05 21:31:57 +0300

    Implement methods for binoms

--- src/fintamath/expressions/binary/PowExpression.cpp
+++ src/fintamath/expressions/binary/PowExpression.cpp
@@ -1,11 +1,17 @@
 #include "fintamath/expressions/binary/PowExpression.hpp"
 
 #include "fintamath/expressions/Expression.hpp"
+#include "fintamath/expressions/polynomial/IPolynomExpression.hpp"
+#include "fintamath/expressions/polynomial/MulExpression.hpp"
+#include "fintamath/expressions/polynomial/SumExpression.hpp"
 #include "fintamath/expressions/unary/InvExpression.hpp"
 // #include "fintamath/expressions/polynomial/MulExpression.hpp"
 #include "fintamath/expressions/unary/NegExpression.hpp"
+#include "fintamath/functions/FunctionArguments.hpp"
 #include "fintamath/functions/powers/Pow.hpp"
+#include "fintamath/meta/Converter.hpp"
 #include "fintamath/numbers/Integer.hpp"
+#include "fintamath/numbers/IntegerFunctions.hpp"
 #include "fintamath/numbers/NumberConstants.hpp"
 #include <memory>
 
@@ -34,24 +40,6 @@ MathObjectPtr PowExpression::simplify(bool isPrecise) const {
     if (*rhsPtr == NEG_ONE) {
       return InvExpression(lhsPtr->clone()).simplify(isPrecise);
     }
-
-    Integer rhs = *rhsPtr;
-
-    bool isInverted = false;
-    if (rhs < ZERO) {
-      isInverted = true;
-      rhs = -rhs;
-    }
-
-    /*MulExpression mul(MulExpression::ArgumentsPtrVector{});
-    for (size_t i = 0; i < rhs; i++) {
-      mul.addElement(exprObj->lhsInfo);
-    }
-
-    if (isInverted) {
-      return InvExpression(mul.simplify()).simplify();
-    }
-    return mul.simplify();*/
   }
 
   if (is<INumber>(exprObj->lhsChild) && is<INumber>(exprObj->rhsChild)) {
@@ -75,10 +63,10 @@ IMathObject *PowExpression::simplify() {
       return lhsPtr;
     }
     if (*rhsPtr == NEG_ONE) {
-      return nullptr;
+      MathObjectPtr invExpr = std::make_unique<InvExpression>(std::move(lhsChild));
+      simplifyExpr(invExpr);
+      return invExpr.release();
     }
-
-    // TODO: add logic for (x+y)^10
   }
 
   if (function->doArgsMatch({*lhsChild, *rhsChild})) {
@@ -90,6 +78,97 @@ IMathObject *PowExpression::simplify() {
 void PowExpression::invert() {
   lhsChild = std::make_unique<NegExpression>(std::move(lhsChild));
   simplifyExpr(lhsChild);
+}
+
+MathObjectPtr PowExpression::polynomialSimplify() {
+  auto *simplExpr = simplify();
+  if (!is<PowExpression>(simplExpr)) {
+    return MathObjectPtr(simplExpr);
+  }
+  auto *powExpr = cast<PowExpression>(simplExpr);
+
+  if (auto *mulExpr = cast<MulExpression>(powExpr->lhsChild.get())) {
+    mulExpr->setPow(powExpr->rhsChild);
+    return MathObjectPtr(powExpr->lhsChild.release());
+  }
+
+  const auto *rhs = cast<Integer>(powExpr->rhsChild.get());
+  if (auto *sumExpr = cast<SumExpression>(powExpr->lhsChild.get()); sumExpr && rhs) {
+    *powExpr->lhsChild.get() = *sumPolynomSimplify(std::move(*sumExpr), *rhs);
+    return MathObjectPtr(powExpr->lhsChild.release());
+  }
+
+  return MathObjectPtr(powExpr);
+}
+
+// TODO: move to IntegerFunctions
+Integer combinations(const Integer &n, const Integer &k) {
+  return factorial(n) / (factorial(k) * factorial(n - k));
+}
+
+// TODO: move to IntegerFunctions
+Integer split(const Integer &n, const std::vector<Integer> &k_values) {
+  auto result = factorial(n);
+  for (const auto &k : k_values) {
+    result /= factorial(k);
+  }
+  return result;
+}
+
+Integer PowExpression::generateNextNumber(Integer n) {
+  Integer u = n & -n;
+  Integer v = u + n;
+  n = v + (((v ^ n) / u) >> 2);
+  return n;
+}
+
+Integer PowExpression::generateFirstNum(const Integer &countOfOne) {
+  Integer n = 0;
+  for (int i = 0; i < countOfOne; i++) {
+    n = n << 1 | 1;
+  }
+  return n;
+}
+
+std::vector<Integer> PowExpression::generateSplit(Integer bitNumber, const Integer &variableCount) {
+  std::vector<Integer> result;
+  Integer counter = 0;
+  while (result.size() < variableCount) {
+    if (bitNumber % 2 == 1) {
+      counter++;
+    }
+    if (bitNumber % 2 == 0) {
+      result.emplace_back(counter);
+      counter = 0;
+    }
+    bitNumber >>= 1;
+  }
+  return result;
+}
+
+IMathObject *PowExpression::sumPolynomSimplify(const SumExpression &sumExpr, const Integer &pow) {
+  auto polynom = sumExpr.getPolynom();
+  ArgumentsPtrVector newPolynom;
+  Integer variableCount = polynom.size();
+  Integer bitNumber = generateFirstNum(pow);
+  for (int i = 0; i < combinations(pow + variableCount - 1, pow); i++) {
+    auto vectOfPows = generateSplit(bitNumber, variableCount);
+    bitNumber = generateNextNumber(bitNumber);
+
+    ArgumentsPtrVector mulExprPolynom;
+    mulExprPolynom.emplace_back(std::make_unique<Integer>(split(pow, vectOfPows)));
+    for (size_t j = 0; j < variableCount; j++) {
+      auto powExpr = std::make_unique<PowExpression>(polynom[j]->clone(), vectOfPows[j].clone());
+      mulExprPolynom.emplace_back(powExpr->polynomialSimplify());
+    }
+    MathObjectPtr mulExpr = std::make_unique<MulExpression>(std::move(mulExprPolynom));
+    simplifyExpr(mulExpr);
+    newPolynom.emplace_back(std::move(mulExpr));
+  }
+
+  MathObjectPtr newSumExpr = std::make_unique<SumExpression>(std::move(newPolynom));
+  simplifyExpr(newSumExpr);
+  return newSumExpr.release();
 }
 
 }
--- src/fintamath/expressions/binary/PowExpression.hpp
+++ src/fintamath/expressions/binary/PowExpression.hpp
@@ -3,6 +3,7 @@
 #include "fintamath/core/IMathObject.hpp"
 #include "fintamath/expressions/IInvertable.hpp"
 #include "fintamath/expressions/binary/IBinaryExpression.hpp"
+#include "fintamath/expressions/polynomial/SumExpression.hpp"
 
 namespace fintamath {
 
@@ -12,9 +13,20 @@ public:
 
   MathObjectPtr simplify(bool isPrecise) const override;
 
+  MathObjectPtr polynomialSimplify();
+
   void invert() override;
 
 protected:
   IMathObject *simplify() override;
+
+private:
+  static IMathObject *sumPolynomSimplify(const SumExpression &sumExpr, const Integer &pow);
+
+  static Integer generateNextNumber(Integer n);
+
+  static Integer generateFirstNum(const Integer &countOfOne);
+
+  static std::vector<Integer> generateSplit(Integer bitNumber, const Integer &variableCount);
 };
 }
