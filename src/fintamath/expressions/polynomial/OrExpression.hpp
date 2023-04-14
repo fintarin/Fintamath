a222e4a9749eca1c83921286d7bd922ad02a9aa0
Semen <sem1235463782@gmail.com>
2023-04-15 01:39:42 +0300

    Change OrExpression toString logic

--- src/fintamath/expressions/polynomial/OrExpression.hpp
+++ src/fintamath/expressions/polynomial/OrExpression.hpp
@@ -11,6 +11,8 @@ public:
 
   ArgumentPtr logicNegate() const override;
 
+  string childToString(const ArgumentPtr &child, bool isFirst) const override;
+
 protected:
   // TODO: refactor, implement other logic functionaluty
   ArgumentPtr preSimplify(size_t lhsChildNum, size_t rhsChildNum) const override;
