46d8d1ce6e358c4bf274a1111ca880671a5130ba
ProtasSemyon <sem1235463782@gmail.com>
2023-04-15 11:17:21 +0300

    Add equations tests

--- tests/src/expressions/ExpressionTests.cpp
+++ tests/src/expressions/ExpressionTests.cpp
@@ -302,6 +302,12 @@ TEST(ExpressionTests, stringConstructorTest) {
   EXPECT_EQ(Expression("-x - 10 = 0").toString(), "x + 10 = 0");
   EXPECT_EQ(Expression("2x = 0").toString(), "x = 0");
   EXPECT_EQ(Expression("2x - 10 = 0").toString(), "x - 5 = 0");
+  EXPECT_EQ(Expression("-a > 0").toString(), "a < 0");
+  EXPECT_EQ(Expression("-2a + 5 > 6").toString(), "a + 1/2 < 0");
+  EXPECT_EQ(Expression("-2a+5>6").toString(), "a - 1/2 < 0");
+  EXPECT_EQ(Expression("-6x^2 + 4x - 20 = 15x - 9").toString(), "x^2 + 11/6 x + 11/6 = 0");
+  EXPECT_EQ(Expression("2 a^5 b - 4 a b^5  = 0"), "a^5 b - 2 a b^5 = 0");
+  EXPECT_EQ(Expression("-2 a^5 b + 4 a b^5  = 0"), "a^5 b - 2 a b^5 = 0");
 
   EXPECT_EQ(Expression("a'").toString(), "1");
   EXPECT_EQ(Expression("(a+a)'").toString(), "(2 a)'");
