4034846246a02daf118c6478418c345ef4116d9a
ProtasSemyon <sem1235463782@gmail.com>
2023-04-19 19:15:11 +0300

    Add test

--- tests/src/FintamathTests.cpp
+++ tests/src/FintamathTests.cpp
@@ -29,7 +29,6 @@ TEST(FintamathTests, fintamathTests) {
   Expression y("y");
 
   expr = eqv(x * x + y * y * y, x * y);
-
   EXPECT_EQ(expr.toString(), "x^2 - x y + y^3 = 0");
 
   expr = eqv(x * x + y * y, x * y);
@@ -38,14 +37,39 @@ TEST(FintamathTests, fintamathTests) {
   expr = eqv(pow(x, 2) - 10, 39);
   EXPECT_EQ(solve(expr).toString(), "x = -7 | x = 7");
 
+  expr = Expression("-4x^2 + 28x - 49 = 0");
+  EXPECT_EQ(solve(expr).toString(), "x = 7/2");
+
   expr = Expression("-3x^2 + 28x - 49 = 0");
   EXPECT_EQ(solve(expr).precise(2).toString(), "x = 2.3 | x = 7");
 
   expr = Expression("-4x^2 + 28x - 49 = 0");
   EXPECT_EQ(solve(expr).precise(2).toString(), "x = 3.5");
 
-  expr = Expression("-4x^2 + 28x - 49 = 0");
-  EXPECT_EQ(solve(expr).toString(), "x = 7/2");
+  expr = Expression("sin(x) + cos(x) = 0");
+  EXPECT_EQ(solve(expr).precise(2).toString(), "sin(x) + cos(x) = 0");
+
+  {
+    Expression expr1("(x+1)^6");
+    EXPECT_EQ(expr1.toString(), "x^6 + 6 x^5 + 15 x^4 + 20 x^3 + 15 x^2 + 6 x + 1");
+
+    Expression expr2 = solve(expr1);
+    EXPECT_EQ(expr2.toString(), "x^6 + 6 x^5 + 15 x^4 + 20 x^3 + 15 x^2 + 6 x + 1");
+
+    Expression expr3 = expr2.precise(10);
+    EXPECT_EQ(expr3.toString(), "x^6 + 6 x^5 + 15 x^4 + 20 x^3 + 15 x^2 + 6 x + 1");
+  }
+
+  {
+    Expression expr1("(x+1)^7");
+    EXPECT_EQ(expr1.toString(), "x^7 + 7 x^6 + 21 x^5 + 35 x^4 + 35 x^3 + 21 x^2 + 7 x + 1");
+
+    Expression expr2 = solve(expr1);
+    EXPECT_EQ(expr2.toString(), "x^7 + 7 x^6 + 21 x^5 + 35 x^4 + 35 x^3 + 21 x^2 + 7 x + 1");
+
+    Expression expr3 = expr2.precise(10);
+    EXPECT_EQ(expr3.toString(), "x^7 + 7 x^6 + 21 x^5 + 35 x^4 + 35 x^3 + 21 x^2 + 7 x + 1");
+  }
 
   // TODO trigonometry
   // expr = Expression("sin2^2 + cos2^2 = 1");
