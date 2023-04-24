75d230d18d55e7a724d1ec15d781e7bbe98c5717
fintarin <valukevichv@gmail.com>
2023-04-24 18:39:50 +0300

    Remove copyProperties

--- src/fintamath/expressions/IExpression.cpp
+++ src/fintamath/expressions/IExpression.cpp
@@ -152,12 +152,6 @@ void IExpression::postSimplifyChild(ArgumentPtr &child) {
   simplifyConstant(child);
 }
 
-void IExpression::copyPropertiesToChild(ArgumentPtr &lhs, const ArgumentPtr &rhs) {
-  if (auto lhsExpr = cast<IExpression>(lhs)) {
-    lhs = lhsExpr->copyPropertiesAbstract(rhs);
-  }
-}
-
 ArgumentPtr IExpression::callFunction(const IFunction &func, const ArgumentsPtrVector &argPtrs) {
   ArgumentsRefVector args;
   bool areArgumentsPrecise = true;
