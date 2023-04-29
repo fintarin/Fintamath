1ac1b4f444f416aa7b18589b321e9c52a84a81d5
Semen <sem1235463782@gmail.com>
2023-04-30 00:37:33 +0300

    Move comments in PowExpression from hpp to cpp

--- src/fintamath/expressions/binary/PowExpression.hpp
+++ src/fintamath/expressions/binary/PowExpression.hpp
@@ -24,16 +24,12 @@ private:
 
   ArgumentPtr mulSimplify() const;
 
-  // start set of methods for Multinomial theorem
-  // https://en.wikipedia.org/wiki/Multinomial_theorem
-  // https://en.wikipedia.org/wiki/Stars_and_bars_(combinatorics)
   static ArgumentPtr sumPolynomSimplify(const ArgumentPtr &sumExpr, Integer powValue);
 
   static Integer generateNextNumber(Integer n);
 
   static Integer generateFirstNum(const Integer &countOfOne);
 
-  static std::vector<Integer> generateSplit(Integer bitNumber, const Integer &variableCount);
-  // end set of methods for Newton's Polynomial decomposition
+  static std::vector<Integer> getPartition(Integer bitNumber, const Integer &variableCount);
 };
 }
