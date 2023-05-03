15ada6eab013845fb4aae72d5b6146d69706a74b
fintarin <valukevichv@gmail.com>
2023-05-03 12:20:40 +0300

    Fix coverage collections

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -3,17 +3,23 @@
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
   option(${PROJECT_NAME}_enable_coverage "Enable coverage reporting for gcc/clang" OFF)
   if(${PROJECT_NAME}_enable_coverage)
-    target_compile_options(${PROJECT_NAME} PRIVATE --coverage -O0 -g)
-    target_link_libraries(${PROJECT_NAME} PRIVATE gcov)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage -O0 -g")
 
     add_custom_target(
       ${PROJECT_NAME}_coverage
       COMMAND cd build && ctest -CDebug && cd ..
       COMMAND mkdir -p build/coverage
-      COMMAND gcovr -r . -e ".*build/.*" -e ".*tests/.*" --exclude-throw-branches --exclude-unreachable-branches
-              --exclude-noncode-lines --xml -o "${CMAKE_BINARY_DIR}/coverage/coverage.xml"
-      COMMAND gcovr -r . -e ".*build/.*" -e ".*tests/.*" --exclude-throw-branches --exclude-unreachable-branches
-              --exclude-noncode-lines --sonarqube -o "${CMAKE_BINARY_DIR}/coverage/coverage_sonar.xml"
+      COMMAND
+        gcovr -r . -e ".*build/.*" -e ".*tests/.*" -e ".*thirdparty/.*" --exclude-throw-branches
+        --exclude-unreachable-branches --exclude-noncode-lines --html-details -o
+        "${CMAKE_BINARY_DIR}/coverage/coverage.html"
+      COMMAND
+        gcovr -r . -e ".*build/.*" -e ".*tests/.*" -e ".*thirdparty/.*" --exclude-throw-branches
+        --exclude-unreachable-branches --exclude-noncode-lines --xml -o "${CMAKE_BINARY_DIR}/coverage/coverage.xml"
+      COMMAND
+        gcovr -r . -e ".*build/.*" -e ".*tests/.*" -e ".*thirdparty/.*" --exclude-throw-branches
+        --exclude-unreachable-branches --exclude-noncode-lines --sonarqube -o
+        "${CMAKE_BINARY_DIR}/coverage/coverage_sonar.xml"
       DEPENDS ${PROJECT_NAME}_tests
       WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
   endif()
