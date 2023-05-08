71dd408edca0ad4351302713a5bfb3bce881ca8e
fintarin <valukevichv@gmail.com>
2023-05-08 12:28:48 +0300

    Refactor Abs function

--- src/fintamath/functions/arithmetic/Abs.cpp
+++ src/fintamath/functions/arithmetic/Abs.cpp
@@ -1,19 +1,12 @@
 #include "fintamath/functions/arithmetic/Abs.hpp"
 
-#include "fintamath/numbers/Integer.hpp"
-#include "fintamath/numbers/NumberConstants.hpp"
 #include "fintamath/numbers/NumberFunctions.hpp"
 
 namespace fintamath {
 
 std::unique_ptr<IMathObject> Abs::call(const ArgumentsRefVector &argsVect) const {
   const auto &rhs = cast<INumber>(argsVect.front().get());
-
-  if (rhs < ZERO) {
-    return -rhs;
-  }
-
-  return rhs.clone();
+  return abs(rhs);
 }
 
 }
