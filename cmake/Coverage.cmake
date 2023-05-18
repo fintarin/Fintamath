3de8e71f456a8e3cf7840831dfbdcf0cdc6e7082
fintarin <valukevichv@gmail.com>
2023-05-18 20:18:11 +0300

    Remove -g flag from coverage

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -3,7 +3,7 @@
 if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
   option(${PROJECT_NAME}_enable_coverage "Enable coverage reporting for gcc/clang" OFF)
   if(${PROJECT_NAME}_enable_coverage)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-instr-generate -fcoverage-mapping -O0 -g")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-instr-generate -fcoverage-mapping -O0")
 
     add_custom_target(
       ${PROJECT_NAME}_coverage
