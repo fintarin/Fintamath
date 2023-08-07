f727c6a007f125621c3e0f2e8f6c4fcc5509c653
fintarin <valukevichv@gmail.com>
2023-08-07 22:21:36 +0300

    Refactor powers functions

--- src/fintamath/functions/powers/Exp.cpp
+++ src/fintamath/functions/powers/Exp.cpp
@@ -1,11 +1,15 @@
 #include "fintamath/functions/powers/Exp.hpp"
 
-#include "fintamath/numbers/RealFunctions.hpp"
+#include "fintamath/functions/powers/Pow.hpp"
+#include "fintamath/literals/constants/E.hpp"
 
 namespace fintamath {
 
 std::unique_ptr<IMathObject> Exp::call(const ArgumentsRefVector &argsVect) const {
-  return exp(convert<Real>(argsVect.front().get())).toMinimalObject();
+  static const E lhs;
+  const auto &rhs = argsVect.front().get();
+
+  return Pow()(lhs, rhs);
 }
 
 }
