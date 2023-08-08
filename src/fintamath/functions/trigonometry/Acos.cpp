193a0e3f5182852980e07035397e4687b75e96e6
fintarin <valukevichv@gmail.com>
2023-08-08 18:43:30 +0300

    Simplify constants in trigonometry functions

--- src/fintamath/functions/trigonometry/Acos.cpp
+++ src/fintamath/functions/trigonometry/Acos.cpp
@@ -1,11 +1,27 @@
 #include "fintamath/functions/trigonometry/Acos.hpp"
 
+#include "fintamath/exceptions/UndefinedException.hpp"
+#include "fintamath/functions/arithmetic/Div.hpp"
+#include "fintamath/functions/arithmetic/Neg.hpp"
+#include "fintamath/literals/constants/Pi.hpp"
 #include "fintamath/numbers/RealFunctions.hpp"
 
 namespace fintamath {
 
 std::unique_ptr<IMathObject> Acos::call(const ArgumentsRefVector &argsVect) const {
   const auto &rhs = cast<INumber>(argsVect.front().get());
+
+  if (rhs == Integer(-1)) {
+    return Pi().toMinimalObject();
+  }
+
+  if (rhs == Integer(0)) {
+    return makeExpr(Div(), Pi(), Integer(2))->toMinimalObject();
+  }
+
+  if (rhs == Integer(1)) {
+    return Integer(0).clone();
+  }
 
   return multiAcosSimpl(rhs);
 }
@@ -23,13 +39,22 @@ std::unique_ptr<IMathObject> Acos::multiAcosSimpl(const INumber &rhs) {
     });
 
     outMultiAcos.add<Real>([](const Real &inRhs) {
-      return acos(inRhs).toMinimalObject();
+      return acosSimpl(inRhs);
     });
 
     return outMultiAcos;
   }();
 
   return multiAcos(rhs);
+}
+
+std::unique_ptr<IMathObject> Acos::acosSimpl(const Real &rhs) {
+  try {
+    return acos(rhs).toMinimalObject();
+  }
+  catch (const UndefinedException &) {
+    return {};
+  }
 }
 
 }
--- tests/src/functions/trigonometry/AcosTests.cpp
+++ tests/src/functions/trigonometry/AcosTests.cpp
@@ -7,6 +7,7 @@
 #include "fintamath/functions/arithmetic/UnaryPlus.hpp"
 #include "fintamath/literals/Variable.hpp"
 #include "fintamath/numbers/Rational.hpp"
+#include "fintamath/numbers/Real.hpp"
 
 using namespace fintamath;
 
@@ -21,17 +22,21 @@ TEST(AcosTests, getFunctionTypeTest) {
 }
 
 TEST(AcosTests, callTest) {
-  EXPECT_EQ(f(Integer(1))->toString(), "0.0");
-  EXPECT_EQ(f(Integer(0))->toString(),
-            "1.5707963267948966192313216916397514420985846996875529104874722961539082031431045");
+  EXPECT_EQ(f(Integer(-1))->toString(), "Pi");
+  EXPECT_EQ(f(Integer(0))->toString(), "Pi/2");
+  EXPECT_EQ(f(Integer(1))->toString(), "0");
+  EXPECT_EQ(f(Integer(-2))->toString(), "acos(-2)");
+  EXPECT_EQ(f(Integer(2))->toString(), "acos(2)");
+
   EXPECT_EQ(f(Rational(1, 10))->toString(),
             "1.4706289056333368228857985121870581235299087274579233690964484411175055294922419");
   EXPECT_EQ(f(Rational(-1, 5))->toString(),
             "1.7721542475852274106864472438573748523386227810897812967447235396095175777719563");
 
-  EXPECT_EQ(f(Variable("a"))->toString(), "acos(a)");
+  EXPECT_EQ(f(Real("0.5"))->toString(),
+            "1.0471975511965977461542144610931676280657231331250352736583148641026054687620697");
 
-  EXPECT_THROW(f(Integer(10)), UndefinedFunctionException);
+  EXPECT_EQ(f(Variable("a"))->toString(), "acos(a)");
 
   EXPECT_THROW(f(), InvalidInputFunctionException);
   EXPECT_THROW(f(Integer(1), Integer(1), Integer(1)), InvalidInputFunctionException);
