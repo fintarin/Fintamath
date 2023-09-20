0d5a07f9a41defac04c36f7c45ba870e39f21219
fintarin <valukevichv@gmail.com>
2023-09-20 22:57:44 +0300

    Use FINTAMATH_FUNCTION_EXPRESSION in Cos

--- include/fintamath/functions/trigonometry/Cos.hpp
+++ include/fintamath/functions/trigonometry/Cos.hpp
@@ -24,9 +24,6 @@ private:
   static std::unique_ptr<IMathObject> multiCosSimplify(const INumber &rhs);
 };
 
-template <typename... Args>
-std ::unique_ptr<IMathObject> cosExpr(Args &&...args) {
-  static const Cos f;
-  return makeExpr(f, std ::forward<Args>(args)...);
-};
+FINTAMATH_FUNCTION_EXPRESSION(Cos, cosExpr);
+
 }
