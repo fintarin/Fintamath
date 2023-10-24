fc73c26b71d6b5c62c0c728987b360dc6b43162b
fintarin <valukevichv@gmail.com>
2023-10-24 20:56:55 +0300

    Add floor and ceil RationalFunctions

--- src/fintamath/numbers/RationalFunctions.cpp
+++ src/fintamath/numbers/RationalFunctions.cpp
@@ -1,6 +1,28 @@
 #include "fintamath/numbers/RationalFunctions.hpp"
 
+#include "fintamath/numbers/IntegerFunctions.hpp"
+
 namespace fintamath {
+
+Integer floor(const Rational &rhs) {
+  Integer div = rhs.numerator() / rhs.denominator();
+
+  if (rhs.numerator() < 0 && abs(rhs.numerator()) % rhs.denominator() != 0) {
+    div--;
+  }
+
+  return div;
+}
+
+Integer ceil(const Rational &rhs) {
+  Integer div = rhs.numerator() / rhs.denominator();
+
+  if (rhs.numerator() >= 0 && abs(rhs.numerator()) % rhs.denominator() != 0) {
+    div++;
+  }
+
+  return div;
+}
 
 Rational abs(const Rational &rhs) {
   return rhs < 0 ? -rhs : rhs;
--- tests/src/numbers/RationalFunctionsTests.cpp
+++ tests/src/numbers/RationalFunctionsTests.cpp
@@ -4,6 +4,50 @@
 
 using namespace fintamath;
 
+TEST(RationalFunctionsTests, floorTest) {
+  EXPECT_EQ(floor(Rational("-1000.8")).toString(), "-1001");
+  EXPECT_EQ(floor(Rational("-1000.1")).toString(), "-1001");
+  EXPECT_EQ(floor(Rational("-1000")).toString(), "-1000");
+  EXPECT_EQ(floor(Rational("-1.9")).toString(), "-2");
+  EXPECT_EQ(floor(Rational("-1.5")).toString(), "-2");
+  EXPECT_EQ(floor(Rational("-1.3")).toString(), "-2");
+  EXPECT_EQ(floor(Rational("-1")).toString(), "-1");
+  EXPECT_EQ(floor(Rational("-0.9")).toString(), "-1");
+  EXPECT_EQ(floor(Rational("-0.3")).toString(), "-1");
+  EXPECT_EQ(floor(Rational("0")).toString(), "0");
+  EXPECT_EQ(floor(Rational("0.3")).toString(), "0");
+  EXPECT_EQ(floor(Rational("0.9")).toString(), "0");
+  EXPECT_EQ(floor(Rational("1")).toString(), "1");
+  EXPECT_EQ(floor(Rational("1.9")).toString(), "1");
+  EXPECT_EQ(floor(Rational("1.5")).toString(), "1");
+  EXPECT_EQ(floor(Rational("1.3")).toString(), "1");
+  EXPECT_EQ(floor(Rational("1000")).toString(), "1000");
+  EXPECT_EQ(floor(Rational("1000.1")).toString(), "1000");
+  EXPECT_EQ(floor(Rational("1000.8")).toString(), "1000");
+}
+
+TEST(RationalFunctionsTests, ceilTest) {
+  EXPECT_EQ(ceil(Rational("-1000.8")).toString(), "-1000");
+  EXPECT_EQ(ceil(Rational("-1000.1")).toString(), "-1000");
+  EXPECT_EQ(ceil(Rational("-1000")).toString(), "-1000");
+  EXPECT_EQ(ceil(Rational("-1.9")).toString(), "-1");
+  EXPECT_EQ(ceil(Rational("-1.5")).toString(), "-1");
+  EXPECT_EQ(ceil(Rational("-1.3")).toString(), "-1");
+  EXPECT_EQ(ceil(Rational("-1")).toString(), "-1");
+  EXPECT_EQ(ceil(Rational("-0.9")).toString(), "0");
+  EXPECT_EQ(ceil(Rational("-0.3")).toString(), "0");
+  EXPECT_EQ(ceil(Rational("0")).toString(), "0");
+  EXPECT_EQ(ceil(Rational("0.3")).toString(), "1");
+  EXPECT_EQ(ceil(Rational("0.9")).toString(), "1");
+  EXPECT_EQ(ceil(Rational("1")).toString(), "1");
+  EXPECT_EQ(ceil(Rational("1.9")).toString(), "2");
+  EXPECT_EQ(ceil(Rational("1.5")).toString(), "2");
+  EXPECT_EQ(ceil(Rational("1.3")).toString(), "2");
+  EXPECT_EQ(ceil(Rational("1000")).toString(), "1000");
+  EXPECT_EQ(ceil(Rational("1000.1")).toString(), "1001");
+  EXPECT_EQ(ceil(Rational("1000.8")).toString(), "1001");
+}
+
 TEST(RationalFunctionsTests, absTest) {
   EXPECT_EQ(abs(Rational(-1, 100)).toString(), "1/100");
   EXPECT_EQ(abs(Rational(1, -100)).toString(), "1/100");
