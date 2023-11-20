4f66bc00af456a18dcc11f920cc5a8be806ccd8c
fintarin <valukevichv@gmail.com>
2023-11-20 14:32:21 +0300

    Refactor ExpressionUtils

--- src/fintamath/expressions/ExpressionUtils.cpp
+++ src/fintamath/expressions/ExpressionUtils.cpp
@@ -18,6 +18,126 @@
 
 namespace fintamath {
 
+bool isExpression(const IMathObject &arg) {
+  return is<IExpression>(arg);
+}
+
+bool isInfinity(const ArgumentPtr &arg) {
+  return is<Inf>(arg) || is<NegInf>(arg) || is<ComplexInf>(arg);
+}
+
+bool isNegated(const ArgumentPtr &arg) {
+  if (isNegativeNumber(arg)) {
+    return true;
+  }
+
+  auto expr = cast<IExpression>(arg);
+  if (!expr) {
+    return false;
+  }
+
+  if (is<Add>(expr->getFunction())) {
+    return isNegated(expr->getChildren().front());
+  }
+
+  if (is<Mul>(expr->getFunction())) {
+    return isNegativeNumber(expr->getChildren().front());
+  }
+
+  return false;
+}
+
+bool isNegativeNumber(const ArgumentPtr &arg) {
+  auto argNum = cast<INumber>(arg);
+  return argNum && *argNum < Integer(0);
+}
+
+bool containsIf(const ArgumentPtr &arg, std::invocable<const ArgumentPtr &> auto comp) {
+  if (comp(arg)) {
+    return true;
+  }
+
+  auto expr = cast<IExpression>(arg);
+  if (!expr) {
+    return false;
+  }
+
+  const ArgumentPtrVector &children = expr->getChildren();
+
+  return std::ranges::any_of(children, [comp](const auto &child) {
+    bool res = false;
+
+    if (containsIf(child, comp)) {
+      res = true;
+    }
+
+    return res;
+  });
+}
+
+bool containsChild(const ArgumentPtr &arg, const ArgumentPtr &child) {
+  return containsIf(arg, [&child](const ArgumentPtr &compArg) {
+    return *compArg == *child;
+  });
+}
+
+bool containsVariable(const ArgumentPtr &arg) {
+  return containsIf(arg, [](const ArgumentPtr &compArg) {
+    return is<Variable>(compArg);
+  });
+}
+
+bool containsVariable(const ArgumentPtr &arg, const Variable &var) {
+  return containsIf(arg, [&var](const ArgumentPtr &compArg) {
+    return *compArg == var;
+  });
+}
+
+bool containsInfinity(const ArgumentPtr &arg) {
+  return containsIf(arg, [](const ArgumentPtr &compArg) {
+    return isInfinity(compArg);
+  });
+}
+
+bool containsComplex(const ArgumentPtr &arg) {
+  return containsIf(arg, [](const ArgumentPtr &compArg) {
+    const auto num = cast<INumber>(compArg);
+    return num && num->isComplex();
+  });
+}
+
+ArgumentPtr makePolynom(const IFunction &func, const ArgumentPtrVector &args) {
+  if (args.empty()) {
+    return {};
+  }
+
+  if (args.size() == 1) {
+    return args.front();
+  }
+
+  return makeExpr(func, args);
+}
+
+std::vector<std::string> argumentVectorToStringVector(const ArgumentPtrVector &args) {
+  std::vector<std::string> argStrings(args.size());
+
+  for (auto i : std::views::iota(0U, argStrings.size())) {
+    argStrings[i] = args[i].get()->toString();
+  }
+
+  return argStrings;
+}
+
+ArgumentPtrVector argumentRefVectorToArgumentPtrVector(const ArgumentRefVector &args) {
+  ArgumentPtrVector argsPtrVect;
+
+  for (const auto &arg : args) {
+    argsPtrVect.emplace_back(arg.get().clone());
+  }
+
+  return argsPtrVect;
+}
+
 std::string putInBrackets(const std::string &str) {
   return '(' + str + ')';
 }
@@ -112,126 +232,6 @@ std::string prefixUnaryOperatorToString(const IOperator &oper, const ArgumentPtr
 
 std::string postfixUnaryOperatorToString(const IOperator &oper, const ArgumentPtr &rhs) {
   return operatorChildToString(oper, rhs) + oper.toString();
-}
-
-bool containsIf(const ArgumentPtr &arg, std::invocable<ArgumentPtr> auto comp) {
-  if (comp(arg)) {
-    return true;
-  }
-
-  auto expr = cast<IExpression>(arg);
-  if (!expr) {
-    return false;
-  }
-
-  const ArgumentPtrVector &children = expr->getChildren();
-
-  return std::ranges::any_of(children, [comp](const auto &child) {
-    bool res = false;
-
-    if (containsIf(child, comp)) {
-      res = true;
-    }
-
-    return res;
-  });
-}
-
-bool containsChild(const ArgumentPtr &arg, const ArgumentPtr &child) {
-  return containsIf(arg, [&child](const ArgumentPtr &compArg) {
-    return *compArg == *child;
-  });
-}
-
-bool containsVariable(const ArgumentPtr &arg) {
-  return containsIf(arg, [](const ArgumentPtr &compArg) {
-    return is<Variable>(compArg);
-  });
-}
-
-bool containsVariable(const ArgumentPtr &arg, const Variable &var) {
-  return containsIf(arg, [&var](const ArgumentPtr &compArg) {
-    return *compArg == var;
-  });
-}
-
-bool containsInfinity(const ArgumentPtr &arg) {
-  return containsIf(arg, [](const ArgumentPtr &compArg) {
-    return isInfinity(compArg);
-  });
-}
-
-bool containsComplex(const ArgumentPtr &arg) {
-  return containsIf(arg, [](const ArgumentPtr &compArg) {
-    const auto num = cast<INumber>(compArg);
-    return num && num->isComplex();
-  });
-}
-
-bool isInfinity(const ArgumentPtr &arg) {
-  return is<Inf>(arg) || is<NegInf>(arg) || is<ComplexInf>(arg);
-}
-
-bool isNegated(const ArgumentPtr &arg) {
-  if (isNegativeNumber(arg)) {
-    return true;
-  }
-
-  auto expr = cast<IExpression>(arg);
-  if (!expr) {
-    return false;
-  }
-
-  if (is<Add>(expr->getFunction())) {
-    return isNegated(expr->getChildren().front());
-  }
-
-  if (is<Mul>(expr->getFunction())) {
-    return isNegativeNumber(expr->getChildren().front());
-  }
-
-  return false;
-}
-
-bool isNegativeNumber(const ArgumentPtr &arg) {
-  auto argNum = cast<INumber>(arg);
-  return argNum && *argNum < Integer(0);
-}
-
-std::vector<std::string> argumentVectorToStringVector(const ArgumentPtrVector &args) {
-  std::vector<std::string> argStrings(args.size());
-
-  for (auto i : std::views::iota(0U, argStrings.size())) {
-    argStrings[i] = args[i].get()->toString();
-  }
-
-  return argStrings;
-}
-
-ArgumentPtrVector argumentRefVectorToArgumentPtrVector(const ArgumentRefVector &args) {
-  ArgumentPtrVector argsPtrVect;
-
-  for (const auto &arg : args) {
-    argsPtrVect.emplace_back(arg.get().clone());
-  }
-
-  return argsPtrVect;
-}
-
-ArgumentPtr makePolynom(const IFunction &func, const ArgumentPtrVector &args) {
-  if (args.empty()) {
-    return {};
-  }
-
-  if (args.size() == 1) {
-    return args.front();
-  }
-
-  return makeExpr(func, args);
-}
-
-bool isExpression(const IMathObject &arg) {
-  return is<IExpression>(arg);
 }
 
 }
