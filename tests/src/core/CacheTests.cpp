d5257e5d5c5e81473e236156ed6f598c4b04d4ef
fintarin <valukevichv@gmail.com>
2023-12-21 11:56:53 +0300

    Add Cache

---
+++ tests/src/core/CacheTests.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/core/CoreUtils.hpp"
+
+#include "fintamath/literals/constants/E.hpp"
+#include "fintamath/numbers/Integer.hpp"
+#include "fintamath/numbers/Rational.hpp"
+
+using namespace fintamath;
+
+TEST(CacheTests, atTest) {
+  // TODO: implement
+}
