0a11c41d5b2e7bcaf986f3de21841d0a1e6c345f
fintarin <valukevichv@gmail.com>
2025-07-31 10:29:21 +0400

    Fix builds

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -30,7 +30,6 @@ function(set_project_warnings)
       # Ignored warnings
       /wd4250 # inherits via dominance warn
       /wd4251 # type1 needs to have dll-interface to be used by clients of type2
-      /wd4127 # conditional expression is constant, consider using 'if constexpr' statement instead
   )
 
   set(CLANG_WARNINGS
