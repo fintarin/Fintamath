48f67bd48c946ab9d7ed66d4452a5042b38337c9
fintarin <valukevichv@gmail.com>
2025-07-31 10:29:21 +0400

    Implement constant parsing

--- src/fintamath/constants/IConstant.cpp
+++ src/fintamath/constants/IConstant.cpp
@@ -1,4 +1,5 @@
 #include "fintamath/constants/IConstant.hpp"
+#include "fintamath/core/MathObjectUtils.hpp"
 
 namespace fintamath {
 
@@ -6,6 +7,24 @@ FINTAMATH_INTERFACE_IMPLEMENTATION(IConstant)
 
 std::string IConstant::toString() const noexcept {
   return std::string(getClassStatic()->getName());
+}
+
+std::shared_ptr<IConstant> IConstant::parseConstant(const std::string &str) {
+  NameToConstantMap &nameToConstantMap = getNameToConstantMap();
+  auto parsedConstantIter = nameToConstantMap.find(str);
+  return parsedConstantIter != nameToConstantMap.end() ? parsedConstantIter->second : nullptr;
+}
+
+void IConstant::registerDefaultObject() const {
+  std::string name = toString();
+  auto object = cast<IConstant>(std::shared_ptr(clone()));
+  const auto empaceResult = getNameToConstantMap().emplace(std::move(name), std::move(object));
+  assert(empaceResult.second);
+}
+
+IConstant::NameToConstantMap &fintamath::IConstant::getNameToConstantMap() {
+  static NameToConstantMap nameToConstantMap;
+  return nameToConstantMap;
 }
 
 }
