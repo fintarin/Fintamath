d36e2f1c55e2a0c2bd9d8702c9ae57adfc573c88
fintarin <valukevichv@gmail.com>
2025-07-31 10:29:21 +0400

    Implement IOperator::Priority

--- include/fintamath/expressions/functions/IOperator.hpp
+++ include/fintamath/expressions/functions/IOperator.hpp
@@ -6,6 +6,39 @@ namespace fintamath {
 
 class IOperator : public IFunction {
   FINTAMATH_INTERFACE_BODY(IOperator, IFunction)
+
+public:
+  enum class Priority : uint8_t {
+    Exponentiation, // e.g.  a ^ b
+    PostfixUnary,   // e.g.  a!
+    PrefixUnary,    // e.g.  -a
+    Multiplication, // e.g.  a * b
+    Addition,       // e.g.  a + b
+    Modulo,         // e.g.  a mod b
+    Comparison,     // e.g.  a = b
+    Conjunction,    // e.g.  a & b
+    Disjunction,    // e.g.  a | b
+    Implication,    // e.g.  a -> b
+    Equivalence,    // e.g.  a <-> b
+    Comma,          // e.g.  a , b
+  };
+
+  struct OperatorDeclaration {
+    FunctionDeclaration functionDeclarion;
+    Priority operatorPriority;
+  };
+
+protected:
+  IOperator() = default;
+
+  explicit IOperator(Children inChildren);
+
+public:
+  constexpr const FunctionDeclaration &getFunctionDeclaration() const noexcept final {
+    return getOperatorDeclaration().functionDeclarion;
+  }
+
+  virtual constexpr const OperatorDeclaration &getOperatorDeclaration() const noexcept = 0;
 };
 
 }
