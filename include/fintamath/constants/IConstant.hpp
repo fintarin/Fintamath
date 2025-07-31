13f8e9897521cd4a52168da739e42de855486053
fintarin <valukevichv@gmail.com>
2025-07-31 10:29:21 +0400

    Refactoring in IConstant

--- include/fintamath/constants/IConstant.hpp
+++ include/fintamath/constants/IConstant.hpp
@@ -11,16 +11,14 @@ class IConstant : public IMathObject {
   FINTAMATH_INTERFACE_BODY(IConstant, IMathObject)
 
 private:
-  using NameToConstantMap = std::unordered_map<std::string, std::shared_ptr<IConstant>>;
+  using NameToConstantMap = std::unordered_map<std::string, std::shared_ptr<const IConstant>>;
 
 public:
   virtual constexpr MathObjectClass getValueClass() const noexcept = 0;
 
   virtual std::unique_ptr<IMathObject> getValue(bool shouldApproximate) const noexcept = 0;
 
-  std::string toString() const noexcept override;
-
-  static std::shared_ptr<IConstant> parseConstant(const std::string &str);
+  static std::shared_ptr<const IConstant> parseConstant(const std::string &str);
 
 protected:
   void registerDefaultObject() const override;
