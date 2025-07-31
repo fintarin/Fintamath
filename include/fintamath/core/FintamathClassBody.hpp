749f4ab178f2b4b1016aeeab8b97d7fd1191084d
fintarin <valukevichv@gmail.com>
2025-07-31 10:29:21 +0400

    Make getDefaultObject protected

--- include/fintamath/core/FintamathClassBody.hpp
+++ include/fintamath/core/FintamathClassBody.hpp
@@ -4,38 +4,41 @@
 
 #include "fintamath/core/FintamathInterfaceBody.hpp"
 
-#define FINTAMATH_CLASS_BODY(Class, SuperClass)                    \
-  FINTAMATH_INTERFACE_BODY(Class, SuperClass)                      \
-                                                                   \
-public:                                                            \
-  constexpr MathObjectClass getClass() const noexcept override {   \
-    return getClassStatic();                                       \
-  }                                                                \
-                                                                   \
-  static const Class &getDefaultObjectStatic() noexcept {          \
-    return defaultObject##Class;                                   \
-  }                                                                \
-                                                                   \
-  const Class &getDefaultObject() const noexcept override {        \
-    return getDefaultObjectStatic();                               \
-  }                                                                \
-                                                                   \
-  std::unique_ptr<IMathObject> clone() const & noexcept override { \
-    return std::make_unique<Class>(*this);                         \
-  }                                                                \
-                                                                   \
-  std::unique_ptr<IMathObject> clone() && noexcept override {      \
-    return std::make_unique<Class>(std::move(*this));              \
-  }                                                                \
-                                                                   \
-private:                                                           \
+#define FINTAMATH_CLASS_BODY(Class, SuperClass)                   \
+  FINTAMATH_INTERFACE_BODY(Class, SuperClass)                     \
+                                                                  \
+public:                                                           \
+  constexpr MathObjectClass getClass() const noexcept override {  \
+    return getClassStatic();                                      \
+  }                                                               \
+                                                                  \
+  std::unique_ptr<IMathObject> clone() const & noexcept override; \
+                                                                  \
+  std::unique_ptr<IMathObject> clone() && noexcept override;      \
+                                                                  \
+protected:                                                        \
+  const Class &getDefaultObject() const noexcept override;        \
+                                                                  \
+private:                                                          \
   FINTAMATH_EXPORT static const Class defaultObject##Class;
 
-#define FINTAMATH_CLASS_IMPLEMENTATION(Class)    \
-  FINTAMATH_INTERFACE_IMPLEMENTATION(Class)      \
-                                                 \
-  const Class Class::defaultObject##Class = [] { \
-    Class object;                                \
-    object.registerDefaultObject();              \
-    return object;                               \
-  }();
+#define FINTAMATH_CLASS_IMPLEMENTATION(Class)                    \
+  FINTAMATH_INTERFACE_IMPLEMENTATION(Class)                      \
+                                                                 \
+  const Class Class::defaultObject##Class = [] {                 \
+    Class object;                                                \
+    object.registerDefaultObject();                              \
+    return object;                                               \
+  }();                                                           \
+                                                                 \
+  std::unique_ptr<IMathObject> Class::clone() const & noexcept { \
+    return std::make_unique<Class>(*this);                       \
+  }                                                              \
+                                                                 \
+  std::unique_ptr<IMathObject> Class::clone() && noexcept {      \
+    return std::make_unique<Class>(std::move(*this));            \
+  }                                                              \
+                                                                 \
+  const Class &Class::getDefaultObject() const noexcept {        \
+    return defaultObject##Class;                                 \
+  }
