4405b19ccd2fcaa677ce0bcaa6e5328609610ced
fintarin <valukevichv@gmail.com>
2025-08-09 20:35:13 +0400

    Use parents array for is function

--- include/fintamath/core/InterfaceBody.hpp
+++ include/fintamath/core/InterfaceBody.hpp
@@ -2,23 +2,23 @@
 
 #include "fintamath/core/FintamathExport.hpp"
 
-#define FINTAMATH_INTERFACE_BODY(Class, SuperClass)            \
-  static_assert(std::is_object_v<Class>);                      \
-                                                               \
-public:                                                        \
-  using Super = SuperClass;                                    \
-                                                               \
-  static constexpr MathObjectClass getClassStatic() noexcept { \
-    return &classData##Class;                                  \
-  }                                                            \
-                                                               \
-private:                                                       \
-  FINTAMATH_EXPORT static const detail::MathObjectClassData classData##Class;
-
-#define FINTAMATH_INTERFACE_IMPLEMENTATION(Class)                \
-  static_assert(std::is_object_v<Class>);                        \
-                                                                 \
-  constexpr detail::MathObjectClassData Class::classData##Class{ \
-    #Class,                                                      \
-    Super::getClassStatic(),                                     \
+#define FINTAMATH_INTERFACE_BODY(Class, SuperClass)                               \
+  static_assert(std::is_object_v<Class>);                                         \
+                                                                                  \
+public:                                                                           \
+  using Super = SuperClass;                                                       \
+                                                                                  \
+  static constexpr MathObjectClass getClassStatic() noexcept {                    \
+    return &classData##Class;                                                     \
+  }                                                                               \
+                                                                                  \
+private:                                                                          \
+  FINTAMATH_EXPORT static constexpr detail::MathObjectClassData classData##Class{ \
+    #Class,                                                                       \
+    Super::getClassStatic(),                                                      \
   };
+
+#define FINTAMATH_INTERFACE_IMPLEMENTATION(Class) \
+  static_assert(std::is_object_v<Class>);         \
+                                                  \
+  constexpr detail::MathObjectClassData Class::classData##Class;
