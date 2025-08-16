a57f6b0c6205cfe12a8a694ff09d173298104bce
fintarin <valukevichv@gmail.com>
2025-08-16 12:37:23 +0400

    Implement Sin approximation

--- src/fintamath/functions/trigonometry/Sin.cpp
+++ src/fintamath/functions/trigonometry/Sin.cpp
@@ -1,6 +1,10 @@
 #include "fintamath/functions/trigonometry/Sin.hpp"
 
+#include "fintamath/core/Converter.hpp"
+#include "fintamath/core/MathObjectUtils.hpp"
 #include "fintamath/numbers/INumber.hpp"
+#include "fintamath/numbers/Real.hpp"
+#include "fintamath/numbers/RealFunctions.hpp"
 
 namespace fintamath {
 
@@ -12,5 +16,14 @@ FINTAMATH_FUNCTION_CLASS_IMPLEMENTATION_WITH_MAKE(
     .returnClass = INumber::getClassStatic(),
   }
 )
+
+Shared<IMathObject> Sin::approximateSelf() const {
+  const auto real = convert<Real>(getArguments().front());
+  if (!real) {
+    return nullptr;
+  }
+
+  return makeShared<Real>(sin(*real));
+}
 
 }
