400001d4e1a523d93c37ef418430df5abc269f80
fintarin <valukevichv@gmail.com>
2025-08-16 12:37:23 +0400

    Implement constant parsing

--- include/fintamath/constants/IConstant.hpp
+++ include/fintamath/constants/IConstant.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <unordered_map>
 
 #include "fintamath/core/IMathObject.hpp"
 
@@ -9,12 +10,23 @@ namespace fintamath {
 class IConstant : public IMathObject {
   FINTAMATH_INTERFACE_BODY(IConstant, IMathObject)
 
+private:
+  using NameToConstantMap = std::unordered_map<std::string, std::shared_ptr<IConstant>>;
+
 public:
   virtual constexpr MathObjectClass getValueClass() const noexcept = 0;
 
   virtual std::unique_ptr<IMathObject> getValue(bool shouldApproximate) const noexcept = 0;
 
   std::string toString() const noexcept override;
+
+  static std::shared_ptr<IConstant> parseConstant(const std::string &str);
+
+protected:
+  void registerDefaultObject() const override;
+
+private:
+  static NameToConstantMap &getNameToConstantMap();
 };
 
 }
