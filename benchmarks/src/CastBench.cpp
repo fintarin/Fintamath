6c44f4e2b7ab219bc7326851727bd0b00ab61f4e
fintarin <valukevichv@gmail.com>
2025-08-16 12:37:10 +0400

    Use IConstant in CastBench

--- benchmarks/src/CastBench.cpp
+++ benchmarks/src/CastBench.cpp
@@ -1,28 +1,29 @@
 #include <benchmark/benchmark.h>
 
+#include "fintamath/literals/constants/IConstant.hpp"
 #include "fintamath/numbers/Integer.hpp"
 
 using namespace fintamath;
 
 static void BM_FintamathCast(benchmark::State &state) {
   Integer integ = 1;
-  IMathObject* obj = &integ;
+  IMathObject *obj = &integ;
 
   for (auto _ : state) {
     benchmark::DoNotOptimize(cast<Integer>(obj));
     benchmark::DoNotOptimize(cast<INumber>(obj));
-    benchmark::DoNotOptimize(cast<IArithmetic>(obj));
+    benchmark::DoNotOptimize(cast<IConstant>(obj));
   }
 }
 
 static void BM_DynamicCast(benchmark::State &state) {
   Integer integ = 1;
-  IMathObject* obj = &integ;
+  IMathObject *obj = &integ;
 
   for (auto _ : state) {
-    benchmark::DoNotOptimize(dynamic_cast<Integer*>(obj));
-    benchmark::DoNotOptimize(dynamic_cast<INumber*>(obj));
-    benchmark::DoNotOptimize(dynamic_cast<IArithmetic*>(obj));
+    benchmark::DoNotOptimize(dynamic_cast<Integer *>(obj));
+    benchmark::DoNotOptimize(dynamic_cast<INumber *>(obj));
+    benchmark::DoNotOptimize(dynamic_cast<IConstant *>(obj));
   }
 }
 
