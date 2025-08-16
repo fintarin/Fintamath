8e194bac03a6bcfaf92cfd33fe80e8e7e7905db3
fintarin <valukevichv@gmail.com>
2025-08-16 12:37:23 +0400

    Improve core and math object utils

--- include/fintamath/core/CoreUtils.hpp
+++ include/fintamath/core/CoreUtils.hpp
@@ -3,7 +3,7 @@
 #include <concepts>
 #include <functional>
 
-namespace fintamath {
+namespace fintamath::detail {
 
 template <typename From, typename To>
 concept ConvertibleToAndNotSameAs = std::convertible_to<From, To> && !std::same_as<From, To>;
@@ -14,9 +14,60 @@ concept TupleLike = requires {
 };
 
 template <typename T>
-concept Hashable = requires(const T &v) {
+concept Hashable = requires(T v) {
   std::hash<T>{}(v);
 };
+
+template <typename... Args>
+struct Overload : Args... {
+  using Args::operator()...;
+};
+
+template <typename T>
+using RemoveQualifiers = std::remove_cvref_t<std::remove_pointer_t<T>>;
+
+template <typename From, typename To>
+struct CopyQualifiersFromTo {
+  using Type = RemoveQualifiers<To>;
+};
+
+template <typename From, typename To>
+struct CopyQualifiersFromTo<const From, To> {
+  using Type = const RemoveQualifiers<To>;
+};
+
+template <typename From, typename To>
+struct CopyQualifiersFromTo<From &, To> {
+  using Type = RemoveQualifiers<To> &;
+};
+
+template <typename From, typename To>
+struct CopyQualifiersFromTo<From &&, To> {
+  using Type = RemoveQualifiers<To> &&;
+};
+
+template <typename From, typename To>
+struct CopyQualifiersFromTo<const From &, To> {
+  using Type = const RemoveQualifiers<To> &;
+};
+
+template <typename From, typename To>
+struct CopyQualifiersFromTo<const From &&, To> {
+  using Type = const RemoveQualifiers<To> &&;
+};
+
+template <typename From, typename To>
+struct CopyQualifiersFromTo<From *, To> {
+  using Type = RemoveQualifiers<To> *;
+};
+
+template <typename From, typename To>
+struct CopyQualifiersFromTo<const From *, To> {
+  using Type = const RemoveQualifiers<To> *;
+};
+
+template <typename From, typename To>
+using CopyQualifiersFromToType = CopyQualifiersFromTo<From, To>::Type;
 
 template <typename Number, typename Function>
 void repeat(const Number &num, Function &&func) {
