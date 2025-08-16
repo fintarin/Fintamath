cc6c4adede6678d02b37d78fc86f19815eaa4177
fintarin <valukevichv@gmail.com>
2025-08-16 12:37:23 +0400

    Implement simplify, solve, approximate of Expression

--- src/fintamath/functions/IFunction.cpp
+++ src/fintamath/functions/IFunction.cpp
@@ -4,9 +4,12 @@
 
 #include <fmt/core.h>
 
+#include "fintamath/constants/IConstant.hpp"
+#include "fintamath/core/Converter.hpp"
 #include "fintamath/core/MathObjectUtils.hpp"
 #include "fintamath/core/Tokenizer.hpp"
 #include "fintamath/exceptions/InvalidInputException.hpp"
+#include "fintamath/numbers/Real.hpp"
 #include "fintamath/variables/Variable.hpp"
 
 namespace fintamath {
@@ -66,6 +69,74 @@ const IFunction::FunctionMakers *IFunction::parseFunctionMakers(const std::strin
   return iter != nameToMakersMap.end() ? &iter->second : nullptr;
 }
 
+void IFunction::preSimplify(Argument &arg) {
+  modify(
+    arg,
+    [](const IFunction &func) { return func.preSimplifySelf(); },
+    &preSimplify,
+    [](Argument &) {},
+    FunctionState::PreSimplify
+  );
+}
+
+void IFunction::simplify(Argument &arg) {
+  modify(
+    arg,
+    [](const IFunction &func) { return func.simplifySelf(); },
+    &simplify,
+    &preSimplify,
+    FunctionState::Simplify
+  );
+}
+
+void IFunction::solve(Argument &arg) {
+  modify(
+    arg,
+    [](const IFunction &func) { return func.solveSelf(); },
+    &solve,
+    &simplify,
+    FunctionState::Solve
+  );
+}
+
+void IFunction::approximate(Argument &arg) {
+  if (const auto constant = cast<IConstant>(arg)) {
+    if (auto res = constant->approximateValue()) {
+      arg = res;
+    }
+    return;
+  }
+
+  if (auto real = convert<Real>(arg)) {
+    arg = real;
+    return;
+  }
+
+  modify(
+    arg,
+    [](const IFunction &func) { return func.approximateSelf(); },
+    &approximate,
+    &solve,
+    FunctionState::Solve
+  );
+}
+
+Shared<IMathObject> IFunction::preSimplifySelf() const {
+  return nullptr;
+}
+
+Shared<IMathObject> IFunction::simplifySelf() const {
+  return nullptr;
+}
+
+Shared<IMathObject> IFunction::solveSelf() const {
+  return nullptr;
+}
+
+Shared<IMathObject> IFunction::approximateSelf() const {
+  return nullptr;
+}
+
 void IFunction::registerDefaultObject() const {
   const Declaration &decl = getDeclaration();
 
@@ -89,32 +160,32 @@ void IFunction::initSelf(Arguments inArgs) {
   args = compressArguments(decl, getClass(), std::move(args));
 }
 
-void IFunction::validateArgumentsNonNull(const Arguments &inArgs) {
-  for (size_t i = 0; i < inArgs.size(); i++) {
-    if (!inArgs[i]) {
+void IFunction::validateArgumentsNonNull(const Arguments &args) {
+  for (size_t i = 0; i < args.size(); i++) {
+    if (!args[i]) {
       throw InvalidInputException(fmt::format(R"(Argument #{} is a null)", i));
     }
   }
 }
 
-void IFunction::validateArgumentsMatch(const Declaration &decl, const Arguments &inArgs) {
-  if (!doArgumentsMatch(decl, inArgs)) {
+void IFunction::validateArgumentsMatch(const Declaration &decl, const Arguments &args) {
+  if (!doArgumentsMatch(decl, args)) {
     throw InvalidInputException("Invalid args"); // TODO
   }
 }
 
-bool IFunction::doArgumentsMatch(const Declaration &decl, const Arguments &inArgs) noexcept {
-  return decl.isVariadic ? doArgumentsMatchVariadic(decl, inArgs)
-                         : doArgumentsMatchNonVariadic(decl, inArgs);
+bool IFunction::doArgumentsMatch(const Declaration &decl, const Arguments &args) noexcept {
+  return decl.isVariadic ? doArgumentsMatchVariadic(decl, args)
+                         : doArgumentsMatchNonVariadic(decl, args);
 }
 
-bool IFunction::doArgumentsMatchNonVariadic(const Declaration &decl, const Arguments &inArgs) noexcept {
-  if (decl.argumentClasses.size() != inArgs.size()) {
+bool IFunction::doArgumentsMatchNonVariadic(const Declaration &decl, const Arguments &args) noexcept {
+  if (decl.argumentClasses.size() != args.size()) {
     return false;
   }
 
-  for (size_t i = 0; i < inArgs.size(); i++) {
-    if (!doesArgumentMatch(decl.argumentClasses[i], inArgs[i])) {
+  for (size_t i = 0; i < args.size(); i++) {
+    if (!doesArgumentMatch(decl.argumentClasses[i], args[i])) {
       return false;
     }
   }
@@ -122,12 +193,12 @@ bool IFunction::doArgumentsMatchNonVariadic(const Declaration &decl, const Argum
   return true;
 }
 
-bool IFunction::doArgumentsMatchVariadic(const Declaration &decl, const Arguments &inArgs) noexcept {
-  if (inArgs.empty()) {
+bool IFunction::doArgumentsMatchVariadic(const Declaration &decl, const Arguments &args) noexcept {
+  if (args.empty()) {
     return false;
   }
 
-  for (const auto &inArg : inArgs) {
+  for (const auto &inArg : args) {
     bool matchAny = false;
     for (const auto &declArgClass : decl.argumentClasses) {
       if (doesArgumentMatch(declArgClass, inArg)) {
@@ -144,40 +215,40 @@ bool IFunction::doArgumentsMatchVariadic(const Declaration &decl, const Argument
   return true;
 }
 
-bool IFunction::doesArgumentMatch(MathObjectClass expectedClass, const Argument &inArg) noexcept {
-  const MathObjectClass argClass = inArg->getClass();
+bool IFunction::doesArgumentMatch(MathObjectClass expectedClass, const Argument &arg) noexcept {
+  const MathObjectClass argClass = arg->getClass();
 
   if (is(expectedClass, argClass) || is<Variable>(argClass)) {
     return true;
   }
 
   if (is<IFunction>(argClass)) {
-    const auto &func = cast<IFunction>(*inArg);
+    const auto &func = cast<IFunction>(*arg);
     return is(expectedClass, func.getDeclaration().returnClass);
   }
 
   return false;
 }
 
-IFunction::Arguments IFunction::unwrappArguments(Arguments inArgs) noexcept {
-  for (auto &arg : inArgs) {
+IFunction::Arguments IFunction::unwrappArguments(Arguments args) noexcept {
+  for (auto &arg : args) {
     if (auto unwrappedArg = arg->unwrapp()) {
       arg = std::move(unwrappedArg);
     }
   }
 
-  return inArgs;
+  return args;
 }
 
-IFunction::Arguments IFunction::compressArguments(const Declaration &decl, const MathObjectClass funcClass, Arguments inArgs) noexcept {
+IFunction::Arguments IFunction::compressArguments(const Declaration &decl, const MathObjectClass funcClass, Arguments args) noexcept {
   if (!decl.isVariadic) {
-    return inArgs;
+    return args;
   }
 
   Arguments outArgs;
-  outArgs.reserve(inArgs.size());
+  outArgs.reserve(args.size());
 
-  for (auto &arg : inArgs) {
+  for (auto &arg : args) {
     if (arg->getClass() == funcClass) {
       const auto &argFunc = cast<IFunction>(*arg);
       const Arguments &innerArgs = argFunc.getArguments();
@@ -189,6 +260,66 @@ IFunction::Arguments IFunction::compressArguments(const Declaration &decl, const
   }
 
   return outArgs;
+}
+
+void IFunction::modify(Argument &arg, const ModifySelfCallback &modifySelf, const ModifyCallback &modify, const ModifyCallback &prevModify, FunctionState stateAfterModify) {
+  auto func = cast<IFunction>(arg);
+  if (!func || func->state >= stateAfterModify) {
+    return;
+  }
+
+  prevModify(arg);
+
+  if (arg != func) {
+    func = cast<IFunction>(arg);
+  }
+
+  if (!func) {
+    return;
+  }
+
+  modifyArguments(func, modify);
+  arg = func;
+
+  if (auto res = modifySelf(*func)) {
+    modify(res);
+    arg = res;
+  }
+  else {
+    func->state = stateAfterModify;
+  }
+}
+
+void IFunction::modifyArguments(Shared<IFunction> &func, const ModifyCallback &modify) {
+  const Arguments &oldArgs = func->getArguments();
+  Arguments newArgs;
+  size_t argIndex = 0;
+  bool wasModify = false;
+
+  for (; argIndex < oldArgs.size(); argIndex++) {
+    const Argument &oldArg = oldArgs[argIndex];
+    Argument newArg = oldArg;
+    modify(newArg);
+
+    if (newArg != oldArg) {
+      newArgs = oldArgs;
+      newArgs[argIndex] = oldArg;
+      wasModify = true;
+      break;
+    }
+  }
+
+  if (!wasModify) {
+    return;
+  }
+
+  argIndex++;
+
+  for (; argIndex < newArgs.size(); argIndex++) {
+    modify(newArgs[argIndex]);
+  }
+
+  func = func->makeSelf(std::move(newArgs));
 }
 
 IFunction::NameToFunctionMakersMap &IFunction::getNameToFunctionMakersMap() {
