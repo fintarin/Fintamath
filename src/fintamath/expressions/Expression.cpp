d8abe291d34f222a0b29e2b12ad0afc8fcf0aa3e
fintarin <valukevichv@gmail.com>
2025-08-30 12:50:20 +0400

    Remove IExpression

--- src/fintamath/expressions/Expression.cpp
+++ src/fintamath/expressions/Expression.cpp
@@ -4,16 +4,4 @@ namespace fintamath {
 
 FINTAMATH_CLASS_IMPLEMENTATION(Expression)
 
-const Expression::ExpressionDeclaration Expression::expressionDeclaration = {
-  .domainAndRangeVariants = {{
-    .returnClass = nullptr,
-    .childClasses = {IMathObject::getClassStatic()},
-  }},
-  .isVariadic = false,
-};
-
-const Expression::ExpressionDeclaration &Expression::getExpressionDeclaration() const noexcept {
-  return expressionDeclaration;
-}
-
 }
