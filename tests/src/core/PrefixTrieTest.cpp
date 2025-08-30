9bf8ca40ea71600e27c042e64d45c889cd86bc1c
fintarin <valukevichv@gmail.com>
2025-08-30 15:33:39 +0400

    Implement PrefixTrie for Tokenizer

---
+++ tests/src/core/PrefixTrieTest.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include "fintamath/core/PrefixTrie.hpp"
+
+using namespace fintamath;
+
+TEST(PrefixTrieTests, addTest) {
+  // TODO: implement
+}
+
+TEST(PrefixTrieTests, getPrefixSizeTest) {
+  // TODO: implement
+}
