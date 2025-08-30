c1fe26555f7c09e6ee34427d34fe1b5e22394028
fintarin <valukevichv@gmail.com>
2025-08-30 12:50:20 +0400

    Refactoring in IConstant

--- src/fintamath/constants/IConstant.cpp
+++ src/fintamath/constants/IConstant.cpp
@@ -1,29 +1,27 @@
 #include "fintamath/constants/IConstant.hpp"
+
 #include "fintamath/core/MathObjectUtils.hpp"
+#include "fintamath/core/Tokenizer.hpp"
 
 namespace fintamath {
 
 FINTAMATH_INTERFACE_IMPLEMENTATION(IConstant)
 
-std::string IConstant::toString() const noexcept {
-  return std::string(getClassStatic()->getName());
-}
-
-std::shared_ptr<IConstant> IConstant::parseConstant(const std::string &str) {
-  NameToConstantMap &nameToConstantMap = getNameToConstantMap();
-  auto parsedConstantIter = nameToConstantMap.find(str);
+std::shared_ptr<const IConstant> IConstant::parseConstant(const std::string &str) {
+  const NameToConstantMap &nameToConstantMap = getNameToConstantMap();
+  const auto parsedConstantIter = nameToConstantMap.find(str);
   return parsedConstantIter != nameToConstantMap.end() ? parsedConstantIter->second : nullptr;
 }
 
 void IConstant::registerDefaultObject() const {
   std::string name = toString();
-  auto object = cast<IConstant>(std::shared_ptr(clone()));
+  detail::Tokenizer::registerToken(name);
 
+  auto object = cast<IConstant>(std::shared_ptr(clone()));
   [[maybe_unused]] const auto empaceResult = getNameToConstantMap().emplace(
     std::move(name),
     std::move(object)
   );
-
   assert(empaceResult.second);
 }
 
