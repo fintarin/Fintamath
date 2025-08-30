1328b6bdef90722e8dcf2d84819ec5cb57e5c9eb
fintarin <valukevichv@gmail.com>
2025-08-30 12:50:20 +0400

    Use const& in expression/function getDeclaration

--- src/fintamath/expressions/functions/arithmetic/Add.cpp
+++ src/fintamath/expressions/functions/arithmetic/Add.cpp
@@ -1,10 +1,21 @@
 #include "fintamath/expressions/functions/arithmetic/Add.hpp"
 
 #include "fintamath/core/MathObjectUtils.hpp"
+#include "fintamath/numbers/INumber.hpp"
 
 namespace fintamath {
 
 FINTAMATH_CLASS_IMPLEMENTATION(Add)
+
+const Add::FunctionDeclaration Add::addDeclaration = {
+  .expressionDeclarion = {
+    .domainAndRangeVariants = {{
+      .returnClass = INumber::getClassStatic(),
+      .childClasses = {INumber::getClassStatic(), INumber::getClassStatic()},
+    }}
+  },
+  .functionName = "add",
+};
 
 Add::Add(Children inChildren) : Super(std::move(inChildren)) {
 }
--- src/fintamath/expressions/functions/arithmetic/AddOper.cpp
+++ src/fintamath/expressions/functions/arithmetic/AddOper.cpp
@@ -2,10 +2,21 @@
 
 #include "fintamath/core/MathObjectUtils.hpp"
 #include "fintamath/expressions/functions/arithmetic/Add.hpp"
+#include "fintamath/numbers/INumber.hpp"
 
 namespace fintamath {
 
 FINTAMATH_CLASS_IMPLEMENTATION(AddOper)
+
+const AddOper::FunctionDeclaration addOperDeclaration = {
+  .expressionDeclarion = {
+    .domainAndRangeVariants = {{
+      .returnClass = INumber::getClassStatic(),
+      .childClasses = {INumber::getClassStatic(), INumber::getClassStatic()},
+    }}
+  },
+  .functionName = "+",
+};
 
 std::unique_ptr<IFunction> AddOper::makeFunction(Children inChildren) const {
   return makeObject<Add>(std::move(inChildren));
