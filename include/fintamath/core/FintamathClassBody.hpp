f030a2aaf25092715c5ece03d497af72638e341f
fintarin <valukevichv@gmail.com>
2025-09-06 20:41:27 +0400

    Revert changes with clone method

--- include/fintamath/core/FintamathClassBody.hpp
+++ include/fintamath/core/FintamathClassBody.hpp
@@ -2,53 +2,33 @@
 
 #include <fmt/format.h>
 
-#include "fintamath/core/CoreUtils.hpp"
 #include "fintamath/core/FintamathInterfaceBody.hpp"
-#include "fintamath/exceptions/Exception.hpp"
 
-#define FINTAMATH_CLASS_BODY(Class, SuperClass)                  \
-  FINTAMATH_INTERFACE_BODY(Class, SuperClass)                    \
-                                                                 \
-public:                                                          \
-  constexpr MathObjectClass getClass() const noexcept override { \
-    return getClassStatic();                                     \
-  }                                                              \
-                                                                 \
-  static const Class &getDefaultObjectStatic() noexcept {        \
-    return defaultObject##Class;                                 \
-  }                                                              \
-                                                                 \
-  const Class &getDefaultObject() const noexcept override {      \
-    return getDefaultObjectStatic();                             \
-  }                                                              \
-                                                                 \
-  std::unique_ptr<IMathObject> clone() const &override {         \
-    if constexpr (std::is_copy_constructible_v<Class>) {         \
-      return std::make_unique<Class>(*this);                     \
-    }                                                            \
-    else {                                                       \
-      throwUnableToCloneException();                             \
-    }                                                            \
-  }                                                              \
-                                                                 \
-  std::unique_ptr<IMathObject> clone() && override {             \
-    if constexpr (std::is_copy_constructible_v<Class>) {         \
-      return std::make_unique<Class>(std::move(*this));          \
-    }                                                            \
-    else {                                                       \
-      throwUnableToCloneException();                             \
-    }                                                            \
-  }                                                              \
-                                                                 \
-private:                                                         \
-  [[noreturn]] void throwUnableToCloneException() const {        \
-    throw Exception(fmt::format(                                 \
-      R"(Unable to clone {} as it is not copy constructible)",   \
-      getClass()->getName()                                      \
-    ));                                                          \
-  }                                                              \
-                                                                 \
-private:                                                         \
+#define FINTAMATH_CLASS_BODY(Class, SuperClass)                    \
+  FINTAMATH_INTERFACE_BODY(Class, SuperClass)                      \
+                                                                   \
+public:                                                            \
+  constexpr MathObjectClass getClass() const noexcept override {   \
+    return getClassStatic();                                       \
+  }                                                                \
+                                                                   \
+  static const Class &getDefaultObjectStatic() noexcept {          \
+    return defaultObject##Class;                                   \
+  }                                                                \
+                                                                   \
+  const Class &getDefaultObject() const noexcept override {        \
+    return getDefaultObjectStatic();                               \
+  }                                                                \
+                                                                   \
+  std::unique_ptr<IMathObject> clone() const & noexcept override { \
+    return std::make_unique<Class>(*this);                         \
+  }                                                                \
+                                                                   \
+  std::unique_ptr<IMathObject> clone() && noexcept override {      \
+    return std::make_unique<Class>(std::move(*this));              \
+  }                                                                \
+                                                                   \
+private:                                                           \
   FINTAMATH_EXPORT static const Class defaultObject##Class;
 
 #define FINTAMATH_CLASS_IMPLEMENTATION(Class)    \
