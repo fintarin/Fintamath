f9af84adc67898f6a6c495f523e0fd8569b3d69e
fintarin <valukevichv@gmail.com>
2025-09-06 20:41:27 +0400

    Filter MathObjectClass.hpp in coverage

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -1,20 +1,24 @@
 # Enable coverage
 
 if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-  option(${PROJECT_NAME}_enable_coverage "Enable coverage reporting for gcc/clang" OFF)
+  option(${PROJECT_NAME}_enable_coverage "Enable coverage reporting for clang" OFF)
   if(${PROJECT_NAME}_enable_coverage)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-instr-generate -fcoverage-mapping -O0")
+
+    set(ignored_files_regex 'tests|build|thirdparty|MathObjectClass.hpp')
 
     add_custom_target(
       ${PROJECT_NAME}_coverage
       COMMAND ./bin/fintamath_tests
       COMMAND llvm-profdata merge -o merged.profdata *.profraw
-      COMMAND llvm-cov show --show-branches=count --ignore-filename-regex='tests|build|thirdparty' --instr-profile
+      COMMAND llvm-cov show --show-branches=count --ignore-filename-regex=${ignored_files_regex} --instr-profile
               merged.profdata bin/fintamath_tests > coverage.txt
-      COMMAND llvm-cov export --ignore-filename-regex='tests|build|thirdparty' --instr-profile merged.profdata
+      COMMAND llvm-cov export --ignore-filename-regex=${ignored_files_regex} --instr-profile merged.profdata
               bin/fintamath_tests -format lcov > lcov.info
       COMMAND genhtml --branch-coverage lcov.info -o coverage
       DEPENDS ${PROJECT_NAME}_tests
       WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+
+    unset(ignored_files_regex)
   endif()
 endif()
